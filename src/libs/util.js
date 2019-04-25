40151f47e1e00a287f3f2ff6f4d56f5f103df9fb
zhangchengzhi <zhangchengzhi@g-part.cn>
2019-04-25 14:26:46 +0800

    修复关闭标签时router的取值问题

--- src/libs/util.js
+++ src/libs/util.js
@@ -203,14 +203,22 @@ export const getParams = url => {
  * @param {String} name 当前关闭的标签的name
  */
 export const getNextName = (list, name) => {
-  let res = ''
+  let resObj = {}
+  resObj.query = {}
+  resObj.res = ''
   if (list.length === 2) {
-    res = 'home'
+    resObj.res = 'home'
   } else {
-    if (list.findIndex(item => item.name === name) === list.length - 1) res = list[list.length - 2].name
-    else res = list[list.findIndex(item => item.name === name) + 1].name
+    if (list.findIndex(item => item.name === name) === list.length - 1) {
+      resObj.res = list[list.length - 2].name
+      resObj.query = list[list.length - 2].query
+    }
+    else{
+      resObj.res = list[list.findIndex(item => item.name === name) + 1].name
+      resObj.query = list[list.findIndex(item => item.name === name) + 1].query
+    }
   }
-  return res
+  return resObj
 }
 
 /**
