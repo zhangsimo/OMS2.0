b38cec38c240ebcc64cd03456973d159b30a84d2
lijuanjia <52844525+lijuanjia@users.noreply.github.com>
2019-11-19 09:02:10 +0800

    jialijuan
    期初库存

--- src/router/routers.ts
+++ src/router/routers.ts
@@ -1453,6 +1453,20 @@ export default [
           },
           {
             path:
+              "/systemSetting/InitialNavigation/inventory",
+            name: "inventory",
+            // mark: '59',
+            meta: {
+              hideInMenu: true,
+              title: "期初库存"
+            },
+            component: () =>
+              import(
+                "@/view/system/systemSetting/InitialNavigation/inventory/inventory.vue"
+              )
+          },
+          {
+            path:
               "/system/systemSetting/InitialNavigation/StockLevel/StockLevel",
             name: "StockLevel",
             // mark: '59',
@@ -1515,12 +1529,12 @@ export default [
             meta: {
               hideInMenu: true,
               title: "品牌车型",
-              activeName:'navigationFater'
+              activeName: 'navigationFater'
             },
             component: () =>
               import(
                 "@/view/system/systemSetting/InitialNavigation/brandCarModel/brandCar.vue"
-                )
+              )
           },
           {
             path:
@@ -1562,7 +1576,7 @@ export default [
             component: () =>
               import(
                 "@/view/system/systemSetting/InitialNavigation/storeManagement/storeManagement.vue"
-                )
+              )
           },
           {
             path:
@@ -1576,7 +1590,7 @@ export default [
             component: () =>
               import(
                 "@/view/system/systemSetting/InitialNavigation/setWarehouse/setWarehouse.vue"
-                )
+              )
           },
           {
             path:
@@ -1590,7 +1604,7 @@ export default [
             component: () =>
               import(
                 "@/view/system/systemSetting/InitialNavigation/setBank/setBank.vue"
-                )
+              )
           },
           {
             path: "/system/staff/management",
@@ -1606,7 +1620,7 @@ export default [
               )
           },
           {
-            path:"/system/partsExamine",
+            path: "/system/partsExamine",
             name: "partsExamine",
             // mark: '59',
             meta: {
