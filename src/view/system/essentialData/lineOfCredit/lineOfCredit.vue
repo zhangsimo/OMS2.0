87ba01bd1104713112a0443f750c9907c48a7114
v-HHH <53888576+v-HHH@users.noreply.github.com>
2019-11-19 21:11:35 +0800

    Update lineOfCredit.vue
    first 获取Date

--- src/view/system/essentialData/lineOfCredit/lineOfCredit.vue
+++ src/view/system/essentialData/lineOfCredit/lineOfCredit.vue
@@ -3,9 +3,9 @@
       <section class="Creditbox">
             <div class="db mr10">
               <span>快速查询：</span>
-              <quickDate class="mr10"></quickDate>
+              <quickDate @quickDate="getvalue" class="mr10"></quickDate>
               <span>开始日期：</span>
-              <DatePicker  type="daterange" placement="bottom-start" placeholder="选择日期"
+              <DatePicker @on-change="getDate" type="daterange" placement="bottom-start" placeholder="选择日期"
                           class="w200 mr20">
               </DatePicker>
               <Button type="warning" class="mr10 w90"><Icon custom="iconfont iconchaxunicon icons"/>查询</Button>
@@ -35,12 +35,89 @@
                         title:'序号'
                     },
                     {
-                        title: '姓名',
+                        title: '客户名称',
                         align: 'center',
-                        key: 'userName'
+                        key: ''
                     },
-                ]
+                    {
+                        title: '调整时间',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '调整人',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '调整时应付',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '调整时应收',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '调整时应收应付合计',
+                        align: 'center',
+                        key: '',
+                        width: '125px'
+                    },
+                    {
+                        title: '应收30天金额',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '应收30-60天',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '应收60天以上',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '调整前临时额度',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '调整后临时额度',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '临时额度开始时间',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '临时额度结束时间',
+                        align: 'center',
+                        key: ''
+                    },
+                    {
+                        title: '调整后剩余额度',
+                        align: 'center',
+                        key: ''
+                    },
+                ],
+                Date: ''
             }
+        },
+        methods: {
+          getvalue(data) {
+            this.Date = data
+            window.console.log(data)
+          },
+          getDate(val) {
+            this.Date = val
+            window.console.log(val)
+          }
         }
     }
 </script>
