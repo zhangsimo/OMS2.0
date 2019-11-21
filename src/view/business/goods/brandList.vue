966e1e647b14acf7ea89c4edbd91c5b9c58c0c06
1443559743 <1443559743@qq.com>
2019-11-21 09:40:32 +0800

    dd

--- src/view/business/goods/brandList.vue
+++ src/view/business/goods/brandList.vue
@@ -2,7 +2,18 @@
   <div>
     <Tabs type="card" :animated="false">
         <TabPane label="基本信息">
-          <quickDate></quickDate>
+          <div>
+            <dateget class="mr10"></dateget>
+            <span class="mr10">日期从：</span>
+            <Date-picker
+              type="daterange" 
+              class="w200 mr20" 
+              :options="options3"
+              @on-change="dateChange" 
+              placeholder="请选择日期"
+            >
+            </Date-picker>
+          </div>
         </TabPane>
         <TabPane label="包装规格">
         </TabPane>
@@ -10,12 +21,20 @@
   </div>
 </template>
 <script>
-  import quickDate from '../../../components/getDate/dateget'
+  import dateget from '../../../components/getDate/dateget'
   export default {
     name: 'brandList',
-    components: {quickDate},
+    components: {dateget},
     data() {
       return {
+        // 日期数据
+        options3: {
+          disabledDate (date) {
+            return date && date.valueOf() > Date.now();
+          }
+        },
+        dateList: {},
+        dateArray: [],
       }
     },
     components: {},
@@ -24,6 +43,16 @@
     mounted() {
     },
     methods: {
+      dateChange (value) {
+        // console.log(value)
+        this.dateArray = [value]
+        if(value[0] === ""){
+          this.dateList = {}
+        }else {
+          this.dateList.startApplyTime = value[0] + " " + "00:00:00"
+          this.dateList.endApplyTime = value[1] + " " + "23:59:59"
+        }
+      },
     },
     computed: {
     }
