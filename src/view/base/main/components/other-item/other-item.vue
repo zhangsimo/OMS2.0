6127d0ad5f199099fad28f3b88b2311ee81a03d6
zhangsimo8 <zhangsimo8@sina.com>
2019-11-21 18:43:04 +0800

    提交

--- src/view/base/main/components/other-item/other-item.vue
+++ src/view/base/main/components/other-item/other-item.vue
@@ -1,16 +1,36 @@
 <template>
+  <div>
     <div class="other-item">
       <a class="mr20" href="#">客服</a>
-      <a class="mr20" href="#">反馈</a>
+      <a class="mr20" @click="toImage">反馈</a>
       <Icon class="mr20" type="md-home" size="16"/>
       <Icon class="mr20" type="md-sync" size="16"/>
       <Icon class="mr20" type="md-close" size="16"/>
     </div>
+
+
+  </div>
 </template>
 
 <script>
+    import html2canvas from 'html2canvas'
   export default {
-    name: 'other-item'
+    name: 'other-item',
+      components: {
+          html2canvas
+      },
+      data(){
+        return {
+            htmlUrl: '',
+        }
+      },
+      methods: {
+          toImage() {
+           this.$emit('getImg',{})
+          },
+
+
+      }
   }
 </script>
 
@@ -18,5 +38,13 @@
   .other-item > *{
     color: #333;
     cursor: pointer;
+  }
+  .image_tofile {
+   position: fixed;
+    width:90%;
+    height: 90%;
+    /*top: 10%;*/
+    left: 10%;
+    background-color: rgba(255,255,255,.1);
   }
 </style>
--- src/view/base/main/main.vue
+++ src/view/base/main/main.vue
@@ -10,9 +10,9 @@
         <div class="logo-con" style="font-size: 18px" v-show="collapsed">
           OMS
         </div>
-        <!--<div class="left-search" v-show="!collapsed">-->
+        <div class="left-search" v-show="!collapsed">
           <!--<span>极配订单管理系统</span>-->
-        <!--</div>-->
+        </div>
       </side-menu>
     </Sider>
     <Layout style="overflow: hidden">
@@ -21,7 +21,7 @@
           <user :user-avator="userAvator" :user-data="userData"/>
           <!--<span class="fs12">版本号：{{omsVersion}}</span>-->
           <fullscreen v-model="isFullscreen" style="margin-right: 20px;"/>
-          <other-item></other-item>
+          <other-item @getImg="toImage"></other-item>
           <!--<message_index></message_index>-->
         </header-bar>
       </Header>
@@ -34,8 +34,50 @@
           <Content class="content-wrapper"
                    style="position: absolute;top: 67px;left: 0;right: 0;bottom: 0;overflow-y: auto">
             <keep-alive :include="cacheList">
-              <router-view/>
+              <div class="image_tofile" ref="imageTofile">
+                <router-view/>
+              </div>
             </keep-alive>
+<!--            反馈模态框-->
+            <Modal v-model="screenshot"  fullscreen title="反馈意见">
+              <div style="display: flex;border-bottom: 1px #cccccc solid">
+                <canvas
+                  id="radom"
+                  :width="canvasWidth"
+                  :height="canvasHeight"
+                  @mousedown="canvasDown($event)"
+                  @mousemove="canvasMove($event)"
+                  @mouseup="canvasUp($event)"
+                  @mouseleave="canvasLeave($event)"
+                  ref="canvas"
+                ></canvas>
+                <div style=" flex-flow: row nowrap;">
+                  <div class="writeIn">
+                  <h5 style="margin-bottom: 5px">选择问题</h5>
+                  <Row>
+                    <Col span="8" v-for="(item,index) in queryList" :key="index" style="text-align: center;margin-bottom: 5px">
+                      <span :class="{'active':spanIndex.indexOf(index)>-1}" class="center" @click="add(item,index)">{{item.name}}</span>
+                    </Col>
+                  </Row>
+                  <Input v-model="value17" maxlength="100" show-word-limit type="textarea" :rows="11" placeholder="请提出宝贵意见..." style="width: 100%"/>
+                </div>
+                <div>
+                  <Row style="text-align: center">
+                    <Col span="12">
+                      <Button type="success" @click="drawWithInfo">标记</Button>
+                    </Col>
+                    <Col span="12">
+                      <Button type="error" @click="clear">清除</Button>
+                    </Col>
+                  </Row>
+                </div>
+                </div>
+              </div>
+              <div slot='footer'>
+                <Button type='primary' @click="sureAdd">确定</Button>
+                <Button type='default' @click='screenshot = false'>取消</Button>
+              </div>
+            </Modal>
           </Content>
         </Layout>
       </Content>
@@ -59,6 +101,7 @@
   import CUR_VERSION from '@/config/version'
   import OtherItem from './components/other-item/other-item'
   import MyBreadCrumb from './components/header-bar/mybread-crumb/myBreadCrumb'
+  import html2canvas from 'html2canvas'
 
   export default {
     name: 'Main',
@@ -71,7 +114,8 @@
       Language,
       TagsNav,
       Fullscreen,
-      User
+      User,
+        html2canvas
     },
     data() {
       return {
@@ -80,7 +124,36 @@
         maxLogo,
         isFullscreen: false,
         keyWord: '',
-        omsVersion: CUR_VERSION
+          htmlUrl: '',
+        omsVersion: CUR_VERSION,
+          screenshot:false,
+          queryList:[
+              {name:'车架号查不到' ,value: 1},
+              {name:'零件号查不到' ,value: 2},
+              {name:'缺少车型' ,value: 3},
+              {name:'数据不全' ,value: 4},
+              {name:'数据错误' ,value: 5},
+              {name:'数据不够新' ,value: 6},
+              {name:'速度慢' ,value: 7},
+              {name:'没有唯一性' ,value: 8},
+              {name:'其他' ,value: 9},
+          ],
+          spanIndex:[], //点击意见
+          value17:'', //意见内容
+          canvasWidth:0,
+          canvasHeight:0,
+          context:{}, // canvas对象
+          // 是否处于绘制状态
+          canvasMoveUse: false,
+          // 绘制矩形和椭圆时用来保存起始点信息
+
+          // 储存坐标信息
+          // 背景图片缓存
+          info:'',//判定标记
+          img:'', //实例化canvas图片
+          canvas:'',//实例化canvas
+          query:[]//点击的问题
+
       }
     },
     computed: {
@@ -124,7 +197,66 @@
       ...mapActions([
         'handleLogin'
       ]),
-      turnToPage(name) {
+        //初始化canvas
+        initDraw () {
+            // 初始化画布
+            this.canvas = document.getElementById('radom')
+            this.ctx = this.canvas.getContext('2d')
+            // 初始化背景图片
+            this.img=new Image()
+            this.img.src = this.htmlUrl
+            this.canvasWidth = document.documentElement.clientWidth - 300
+            this.canvasHeight= document.documentElement.clientHeight - 100
+            this.img.onload = () => {
+                this.ctx.drawImage(this.img, 0, 0,this.canvasWidth,this.canvasHeight)
+            }
+        },
+        //添加画笔
+        drawWithInfo(){
+            this.ctx.lineWidth = 1;//设置线的宽度
+            this.info='pen'
+        },
+        //清除画板
+        clear(){
+            this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+            this.img=new Image()
+            this.img.src = this.htmlUrl
+            this.img.onload = () => {
+                this.ctx.drawImage(this.img, 0, 0,this.canvasWidth,this.canvasHeight)
+            }
+        },
+        canvasDown(e) {
+            this.canvasMoveUse = true;
+            const canvasX = e.clientX - e.target.offsetLeft ;
+            const canvasY =( e.clientY - e.target.offsetTop + document.documentElement.scrollTop - 50 );
+            this.ctx.strokeStyle = "red";
+            this.ctx.beginPath() // 移动的起点
+            this.ctx.moveTo(canvasX, canvasY);
+
+        },
+        canvasMove(e) {
+            if (this.canvasMoveUse) {
+                // 只有允许移动时调用
+                if(this.info == 'pen'){
+                    const t = e.target;
+                    let canvasX;
+                    let canvasY;
+                    canvasX = e.clientX - t.offsetLeft;
+                    canvasY = e.clientY - t.offsetTop + document.documentElement.scrollTop - 50;
+                    this.ctx.lineTo(canvasX, canvasY);
+                    this.ctx.stroke();
+                }
+            }
+        },
+        canvasUp(e) {
+            this.canvasMoveUse = false;
+        },
+        canvasLeave(e) {
+            this.canvasMoveUse = false;
+        },
+
+
+        turnToPage(name) {
         // console.log('turToPage',name);
         if (name.indexOf('isTurnByHref_') > -1) {
           window.open(name.split('_')[1])
@@ -153,7 +285,49 @@
           name: item.name,
           query: item.query
         })
-      }
+      },
+        //点击选中意见
+        add(data , idx){
+            let arrIndex = this.spanIndex.indexOf(idx);
+            if(arrIndex>-1){
+                this.spanIndex.splice(arrIndex,1);
+            }else {
+                this.spanIndex.push(idx)
+            }
+            if(this.query.some( item =>item.value == data.value)){
+                this.query = this.query.filter( item => item.value != data.value)
+            }else {
+                this.query.push(data)
+            }
+        },
+        //图片
+        toImage() {
+          if(this.canvas){
+              this.clear()
+          }
+            this.screenshot = true
+            // 第一个参数是需要生成截图的元素,第二个是自己需要配置的参数,宽高等
+            html2canvas(this.$refs.imageTofile, {
+                backgroundColor: null
+            }).then((canvas) => {
+                let url = canvas.toDataURL('image/png');
+                this.htmlUrl = url;
+                if (url){
+
+                }
+                this.initDraw()
+                // 把生成的base64位图片上传到服务器,生成在线图片地址
+                // this.sendUrl();
+            })
+        },
+        //确认添加
+        sureAdd(){
+          let image = this.canvas.toDataURL("image/png")
+          let list = this.$route
+              image = image.replace(/^data:image\/(png|jpg);base64,/, "")
+            console.log(this.query)
+
+        }
     },
     watch: {
       '$route'(newRoute) {
@@ -168,8 +342,53 @@
       this.setTagNavList()
       this.addTag(this.$store.state.app.homeRoute)
       this.setBreadCrumb(this.$route.matched)
+        //初始化canvas
       // 设置初始语言
       // this.setLocal(this.$i18n.locale)
     }
   }
 </script>
+<style scoped lang="less">
+  .image_tofile {
+    height: 100%;
+  }
+  .imgBox {
+    position: fixed;
+    background-color: rgba(0,0,0,.4);
+    z-index: 999;
+    width: 90%;
+    height: 100%;
+    padding: 50px;
+
+  }
+  .writeIn{
+    margin-bottom: 10px;
+    width: 250px;
+    height: 300px;
+    border: 1px #cccccc solid;
+    margin-left: 5px;
+    background-color: rgba(216, 216, 216, 0.3);
+    font-size: 12px;
+    padding: 2px;
+    border-radius: 5px;
+  }
+  .center{
+    display: inline-block;
+    border-radius: 3px;
+    width: 75px;
+    /*border: 1px #333333 solid;*/
+    background-color: #fff;
+  }
+  .center:hover {
+    cursor:pointer;
+    background-color: #2399ff;
+    color: #ffffff;
+  }
+  .active{
+    background-color: #2399ff;
+    color: #ffffff;
+  }
+  .sign{
+
+  }
+</style>
