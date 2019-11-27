d3c1f1097dc3347f36d22abf72fde69de2a8fdaa
zapeng <526744312@qq.com>
2019-11-27 21:08:46 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const router = new Router({
   mode: 'history'
 })
 const LOGIN_PAGE_NAME = 'login'
+const register_PAGE_NAME = 'register'
 
 router.beforeEach((to, from, next) => {
 
@@ -22,13 +23,13 @@ router.beforeEach((to, from, next) => {
 
   LoadingBar.start()
   const token = getToken()
-  if (!token && to.name !== LOGIN_PAGE_NAME) {
+  if (!token && to.name !== LOGIN_PAGE_NAME||!token && to.name !== register_PAGE_NAME) {
     // 未登录且要跳转的页面不是登录页
     next({
       name: LOGIN_PAGE_NAME // 跳转到登录页
     })
-  } 
-  else if (!token && to.name === LOGIN_PAGE_NAME) {
+  }
+  else if (!token && to.name === LOGIN_PAGE_NAME||!token && to.name === register_PAGE_NAME) {
     // 未登陆且要跳转的页面是登录页
     next() // 跳转
   } else if (token && to.name === LOGIN_PAGE_NAME) {
