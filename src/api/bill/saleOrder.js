0332ac67eb533fb0fe622251b80020cede6273b7
yujiayan <yujiayan@g-part.cn>
2019-12-03 20:00:40 +0800

    接口

--- src/api/bill/saleOrder.js
+++ src/api/bill/saleOrder.js
@@ -1,10 +1,26 @@
 import api from '_conf/url'
 import axios from '@/libs/api.request'
+
 // 销售订单明细
 export function getOrderlist(data) {
     return axios.request({
         url: `${api.omsOrder}/sellOrderMain/findByQuery`,
         method: 'post',
         data
+    })
+}
+// 分店名称
+export function getStorelist(data) {
+    return axios.request({
+        url:`${api.authApi}/group/findRootByPid?pId=0`,
+        method: 'get',
+        data
+    })
+}
+// 应收应付管理 
+export function getreceivable () {
+    return axios.request({
+        url:`${api.omsSettle}/receivable/payable/get`,
+        method: 'get',
     })
 }
