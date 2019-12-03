b4a55d4db1a9861ccdcae9b8dc643618e73691c2
yujiayan <yujiayan@g-part.cn>
2019-12-03 20:15:32 +0800

    修改

--- src/view/settlementManagement/components.js
+++ src/view/settlementManagement/components.js
@@ -3,14 +3,6 @@ export function creat(dates,store) {
   let value = [];
   let Branchstore = [];
   let model = '';
-  // let currentTime = new Date();
-  // let currentDate = new Date(currentTime)
-  // let timesStamp = currentDate.getTime();
-  // let currenDay = currentDate.getDay();
-  // let dates = [];
-  // for (let i = 0; i < 7; i++) {
-  //   dates.push(new Date(timesStamp + 24 * 60 * 60 * 1000 * (i - (currenDay + 6) % 7)).toLocaleDateString().replace(/\//g, '-'));
-  // }
   value.push(dates.shift(), dates.pop())
   async function get () {
     let res = await getStorelist({ groupId: store.state.user.userData.groupId });
