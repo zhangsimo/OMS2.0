a6f16d193872e8a6807c66669a8cbc1369764028
bitcat <1653399637@qq.com>
2019-12-05 21:04:17 +0800

    费用等级

--- src/view/goods/plannedPurchaseOrder/components/FeeRegistration.vue
+++ src/view/goods/plannedPurchaseOrder/components/FeeRegistration.vue
@@ -244,10 +244,10 @@ export default class FeeRegistration extends Vue {
       page: this.page.num - 1
     };
     if (this.selectValue == "0") {
-      data.fullName = this.inputval;
+      data.fullName = this.inputval.trim();
     }
     if (this.selectValue == "1") {
-      data.code = this.inputval;
+      data.code = this.inputval.trim();
     }
     let res: any = await api.findPageByguest(params, data);
     if (res.code == 0) {
