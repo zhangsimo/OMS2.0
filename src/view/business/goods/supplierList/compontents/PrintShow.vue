d52d316d17c45124bdf794f62961d3f87d79d383
xuexia_0319 <zxx_970319@163.com>
2019-12-09 11:33:46 +0800

    amend!

---
+++ src/view/business/goods/supplierList/compontents/PrintShow.vue
@@ -0,0 +1,277 @@
+<template>
+  <Modal v-model="printShow" width="1300" closable>
+    <div id="printBox" style="height: 600px">
+      <div class="titler">
+        <Row style="border: 1px #000000 solid">
+          <Col span="12" class="pl10">
+            <h5 style="font-size: 20px;line-height: 44px;border-right: 1px #000000 solid">
+              {{ onelist.orgName }}
+            </h5>
+          </Col>
+          <Col span="12" class="pl10">
+            <p>调拨申请单:</p>
+            <p>No: {{ onelist.serviceId }}</p>
+          </Col>
+        </Row>
+        <Row class="pt10 pd10" style="border: 1px #000000 solid;border-top: none">
+          <Col span="12" class="pl10" style="border-right: 1px #000000 solid">
+            <p><span>地址: {{ onelist.receiveAddress }}</span></p>
+            <p><span>电话: {{ onelist.receiverMobile }}</span></p>
+          </Col>
+          <Col span="12" class="pl10">
+            <p>
+              <span>订单日期:</span><span>{{  }}</span>
+            </p>
+            <p>
+              <span>打印日期:</span>
+              <span>{{  }}</span>
+            </p>
+          </Col>
+        </Row>
+        <Row style="border: 1px #000000 solid;border-top: none">
+          <Col span="8" class="pl10" style="border-right: 1px #000000 solid">
+            <p>
+              <span>客户:</span> <span>{{ onelist.guestName }}</span>
+            </p>
+            <p>
+              <span>地址:</span> <span>{{ onelist.guestAddress }}</span>
+            </p>
+          </Col>
+          <Col span="8" class="pl10" style="border-right: 1px #000000 solid">
+            <p>
+              <span>联系人:</span> <span>{{ onelist.receiver }}</span>
+            </p>
+            <p>
+              <span>票据类型:</span><span>{{  }}</span>
+            </p>
+          </Col>
+          <Col span="8" class="pl10">
+            <p>
+              <span>联系电话:</span><span>{{ onelist.receiverMobile }}</span>
+            </p>
+            <p>
+              <span>结算方式:</span><span>{{ onelist.settleType }}</span>
+            </p>
+          </Col>
+        </Row>
+        <Table
+          resizable
+          size="small"
+          style="margin: 0 auto"
+          width="990"
+          border
+          :columns="columns2"
+          :data="details"
+          class="ml10"
+        ></Table>
+        <Row style="border: 1px #000000 solid">
+          <Col class="pl10" span="8" style="border-right: 1px #000000 solid">
+            <span>合计:</span>
+            <span>{{ onelist.totalAmt | toChies }}</span>
+          </Col>
+          <Col class="pl10" span="8" style="border-right: 1px #000000 solid">
+            <span>总数:</span>
+            <span>{{ onelist.orderQty }}</span>
+          </Col>
+          <Col class="pl10" span="8">
+            <span>合计:</span>
+            <span>{{ onelist.totalAmt }}</span>
+          </Col>
+        </Row>
+        <Row style="border: 1px #000000 solid;border-top: none">
+          <Col span="6" class="pl10" style="border-right: 1px #000000 solid">
+            <span>制单人:</span>
+            <span>{{ onelist.orderMan }}</span>
+          </Col>
+          <Col span="6" class="pl10" style="border-right: 1px #000000 solid">
+            <span>送货人:</span>
+            <span>{{ onelist.deliverer }}</span>
+          </Col>
+          <Col span="6" class="pl10">
+            <span>收货人:</span>
+            <span>{{ onelist.receiver }}</span>
+          </Col>
+        </Row>
+      </div>
+
+      <div></div>
+    </div>
+    <div slot="footer">
+      <Button type="success" @click="print">打印</Button>
+      <Button type="default" @click="printShowfalse">取消</Button>
+    </div>
+  </Modal>
+</template>
+
+<script>
+  import * as api from "_api/procurement/plan";
+  import * as tools from "../../../../../utils/tools";
+  import { pointAdd } from '../../../../../api/AlotManagement/transferringOrder';
+
+  export default {
+    name: "PrintShow",
+    props: ['orderId'],
+    data() {
+      return {
+        printShow: false, //模态框隐藏
+        columns2: [
+          {
+            title: "序号",
+            type: "index",
+            align: "center"
+          },
+          {
+            title: "配件编码",
+            key: "partCode",
+            align: "center"
+          },
+          {
+            title: "配件名称",
+            key: "partName",
+            align: "center"
+          },
+          {
+            title: "品牌",
+            key: "partBrand",
+            align: "center"
+          },
+          {
+            title: "申请数量",
+            key: "applyQty",
+            align: "center"
+          },
+          {
+            title: "备注",
+            key: "remark",
+            align: "center"
+          },
+          {
+            title: "单位",
+            key: "unit",
+            align: "center"
+          },
+          {
+            title: "  OE码",
+            key: "oemCode",
+            align: "center"
+          },
+          {
+            title: "规格",
+            key: "spec",
+            align: "center"
+          },
+          {
+            title: "方向",
+            key: "enterUnitId",
+            align: "center"
+          },
+          {
+            title: "紧销品",
+            key: "isTightPart",
+            align: "center",
+            render: (h ,params) => {
+              let isTight = params.row.isTight
+              let zi = ''
+              if(isTight === 0){
+                zi = "不是"
+              }
+              if(isTight === 1){
+                zi = '是'
+              }
+              return h('span',zi)
+            }
+          },
+          {
+            title: "受理数量",
+            key: "hasAcceptQty",
+            align: "center"
+          },
+          {
+            title: "取消数量",
+            key: "hasCancelQty",
+            align: "center"
+          },
+          {
+            title: "出库数量",
+            key: "hasOutQty",
+            align: "center"
+          },
+          {
+            title: "入库数量",
+            key: "storeShelf",
+            align: "hasInQty"
+          }
+        ],
+        onelist: {}, //打印数据
+        details: [],
+        num: "12323.09",
+        num2: 78723
+      };
+    },
+    methods: {
+      //打印
+      print() {
+        // 1.设置要打印的区域 div的className
+        var newstr = document.getElementById("printBox").innerHTML;
+        // 2. 还原：将旧的页面储存起来，当打印完成后返给给页面。
+        var oldstr = document.body.innerHTML;
+        // 3. 复制给body，并执行window.print打印功能
+        document.body.innerHTML = newstr;
+        window.print();
+        // 重新加载页面，以刷新数据
+        window.location.reload();
+        document.body.innerHTML = oldstr;
+      },
+      async openModal() {
+        if (this.orderId) {
+          let res = await pointAdd({id:this.orderId});
+          if (res.code === 0) {
+            this.printShow = true;
+            this.onelist = res.data;
+            // this.onelist.printDate = tools.transTime(new Date());
+            this.details = res.data.detailVOS;
+          }
+        } else {
+          this.$message.error("至少选择一条信息");
+        }
+      },
+      //取消
+      printShowfalse(){
+        this.printShow = false
+      }
+    }
+  };
+</script>
+
+<style scoped lang="less">
+  .vertical-center-modal {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .ivu-modal {
+      top: 0;
+    }
+  }
+  #printBox {
+    width: 1000px;
+    margin: 0 auto;
+    overflow: hidden;
+    overflow-y: scroll;
+    .titler {
+      padding-left: 10px;
+    }
+  }
+  #printBox::-webkit-scrollbar {
+    display: none;
+  }
+  #printBox {
+    -ms-overflow-style: none;
+  }
+  #printBox::-webkit-scrollbar {
+    width: 0 !important;
+  }
+  #printBox {
+    overflow: -moz-scrollbars-none;
+  }
+</style>
