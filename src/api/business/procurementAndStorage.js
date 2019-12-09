a59f427042c77fcd7904f19cdd19283eb9e9cca3
zhangsimo8 <zhangsimo8@sina.com>
2019-12-09 22:39:56 +0800

    添加配置文件

--- src/api/business/procurementAndStorage.js
+++ src/api/business/procurementAndStorage.js
@@ -3,9 +3,35 @@ import axios from "@/libs/api.request";
 
 export function getLeftList (data={}) {
   return axios.request({
-    url: `${api.omsOrder}/pchsEnterMain/findPageByDynamicQuery`,
+    url: `${api.omsOrder}/pchsEnterMain/findPageByDynamicQuery?size=${data.size}&page=${data.page}`,
     method: 'post',
     data
   })
 }
 
+
+export  function getLeftMoreList( data ={}) {
+  return axios.request({
+    url: `${api.omsOrder}/pchsEnterMain/moreQueryConditions?size=${data.size}&page=${data.page}`,
+    method: 'post',
+    data
+  })
+}
+
+//保存
+export  function saveList( data ={}) {
+  return axios.request({
+    url: `${api.omsOrder}/pchsEnterMain/saveOrUpdate?`,
+    method: 'post',
+    data
+  })
+}
+
+//计算总和
+export  function getCalculate( params ={}) {
+  return axios.request({
+    url: `${api.omsOrder}/pchsEnterMain/findTotalAmt?`,
+    method: 'get',
+    params
+  })
+}
