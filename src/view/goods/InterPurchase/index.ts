6649d53bffc7df4cbcd2bfdf81b7c29f4ef04d8a
bitcat <1653399637@qq.com>
2019-12-20 20:43:01 +0800

    bug ts

--- src/view/goods/InterPurchase/index.ts
+++ src/view/goods/InterPurchase/index.ts
@@ -406,7 +406,7 @@ export default class InterPurchase extends Vue {
           this.saveHandle('formplanref');
           this.mainId = row.id || "";
           this.tableData = row.details || [];
-          this.selectRowState = "";
+          this.selectRowState = null;
           this.serviceId = row.serviceId || "";
           this.isInput = false;
           this.adjustButtonDisable = true;
