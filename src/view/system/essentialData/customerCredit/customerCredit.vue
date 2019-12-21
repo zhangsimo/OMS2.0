0c3fa700d45af41b71f1e06b7bf332257341652b
Mrhe111 <18257377846m@sina.cn>
2019-12-21 15:37:49 +0800

    客户信用管理-必填项输入正确，申请信用界面的确认按钮点击无反应bug修复

--- src/view/system/essentialData/customerCredit/customerCredit.vue
+++ src/view/system/essentialData/customerCredit/customerCredit.vue
@@ -60,7 +60,7 @@
 
 <!--      申请信用额度-->
       <Modal v-model="CreditLineApplicationShow" title="申请信用额度" width="1100">
-        <CreditLineApplication :data="creaditList" :sendMsg="applicationArr" :payable="payable" :quality="quality"></CreditLineApplication>
+        <CreditLineApplication ref="child" :data="creaditList" :sendMsg="applicationArr" :payable="payable" :quality="quality"></CreditLineApplication>
         <div slot='footer'>
           <Button type='primary' @click="Determined">确定</Button>
           <Button type='default' @click="cancel2">取消</Button>
@@ -594,35 +594,41 @@
           },
           //确定申请
           Determined(){
-            let data = {}
-            data.guestId = this.rowMessage.guestId
-            data.orgId = this.rowMessage.orgid
-            data.fixationQuotaTotal = +this.creaditList.applyQuota+this.creaditList.creditLimit || 0 + (+this.creaditList.applyQuota)
-            data.tempQuotaTotal = +this.creaditList.tempQuota + this.creaditList.tempCreditLimit || 0 + (+this.creaditList.tempQuota)
-            data.applyQuota = +this.creaditList.applyQuota || 0
-            data.tempQuota = +this.creaditList.tempQuota || 0
-            data.tempStart = tools.transTime(this.creaditList.tempStart)
-            data.tempEnd = tools.transTime(this.creaditList.tempEnd)
-            data.payableAmt = +this.payable.payableAmt || 0
-            data.tgrade = this.creaditList.tgrade || ''
-            data.thirtyAmt = +this.payable.thirtyAmt || 0
-            data.sixtyAmt = +this.payable.sixtyAmt || 0
-            data.moreSixtyAmt = this.payable.moreSixtyAmt || 0
-            data.afterAdjustQuota = this.creaditList.totalSum
-            data.quotaReason = this.creaditList.quotaReason || ''
-            data.receivableAmt = this.payable.receivableAmt || 0
-            data.totalQuota = (+this.creaditList.applyQuota+this.creaditList.creditLimit) + (+this.creaditList.tempQuota + this.creaditList.tempCreditLimit)
-                             || +data.creaditList.applyQuota + (+data.creaditList.tempQuota)
-            data.addTotalQuota = this.creaditList.tototo || 0
-            data.adjustType = 0
-            console.log(data)
-            save(data).then(res => {
-              if(res.code === 0){
-                this.CreditLineApplicationShow = false
-                this.$Message.warning('保存成功')
-                this.getListTop()
-              }
-            })
+            // this.$refs.child.$refs.form.validate((valid) => {
+            //         if (valid) {
+                      let data = {}
+                        data.guestId = this.rowMessage.guestId
+                        data.orgId = this.rowMessage.orgid
+                        data.fixationQuotaTotal = +this.creaditList.applyQuota+this.creaditList.creditLimit || 0 + (+this.creaditList.applyQuota)
+                        data.tempQuotaTotal = +this.creaditList.tempQuota + this.creaditList.tempCreditLimit || 0 + (+this.creaditList.tempQuota)
+                        data.applyQuota = this.creaditList.applyQuota || 0
+                        data.tempQuota = this.creaditList.tempQuota || 0
+                        data.tempStart = tools.transTime(this.creaditList.tempStart)
+                        data.tempEnd = tools.transTime(this.creaditList.tempEnd)
+                        data.payableAmt = +this.payable.payableAmt || 0
+                        data.tgrade = this.creaditList.tgrade || ''
+                        data.thirtyAmt = +this.payable.thirtyAmt || 0
+                        data.sixtyAmt = +this.payable.sixtyAmt || 0
+                        data.moreSixtyAmt = this.payable.moreSixtyAmt || 0
+                        data.afterAdjustQuota = this.creaditList.totalSum
+                        data.quotaReason = this.creaditList.quotaReason || ''
+                        data.receivableAmt = this.payable.receivableAmt || 0
+                        data.totalQuota = (+this.creaditList.applyQuota+this.creaditList.creditLimit) + (+this.creaditList.tempQuota + this.creaditList.tempCreditLimit)
+                                        || +this.creaditList.applyQuota + (+this.creaditList.tempQuota)
+                        data.addTotalQuota = this.creaditList.tototo || 0
+                        data.adjustType = 0
+                        console.log(data,'data.applyQuota')
+                        save(data).then(res => {
+                          if(res.code === 0){
+                            this.CreditLineApplicationShow = false
+                            this.$Message.warning('保存成功')
+                            this.getListTop()
+                          }
+                        })
+                //     } else {
+                //         this.$Message.error('表单验证失败!');
+                //     }
+                // })
           },
           //确定取消
           cancel2(){
