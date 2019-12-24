1ee493a6bf869af16625dc686fb7b9c4b90d8906
zhangsimo8 <zhangsimo8@sina.com>
2019-12-24 16:00:21 +0800

    添加配置文件

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -73,20 +73,18 @@ class httpRequest {
           // else{
             config.headers['Authorization'] = "Bearer "+Cookies.get(TOKEN_KEY)
             config.params = config.params || {}
-            // let res = JSON.parse(localStorage.getItem('oms2-userList'))
-            // config.params.tenantId = res.tenantId || 0
-            // config.params.shopId = res.shopId || 0
-            // config.params.shopkeeper = res.shopkeeper || 0
-        // if(!config.params.tenantId || !config.params.shopId || !config.params.shopkeeper ) {
             if(localStorage.getItem("oms2-userList") != null) {
-              // config.params.tenantId = 0
-              // if(localStorage.tenantId != undefined) {
-              //   config.params.tenantId = localStorage.tenantId;
-              // }
               let res = JSON.parse(localStorage.getItem('oms2-userList'))
               config.params.tenantId = res.tenantId || 0
               config.params.shopId = res.shopId || 0
               config.params.shopkeeper = res.shopkeeper || 0
+            }
+            if(localStorage.getItem('userScope') != null){
+              let  scope = localStorage.getItem('userScope')
+              console.log(scope , 789)
+              config.params.scope = scope || 'oms'
+            }else{
+              config.params.scope = 'oms'
             }
       }else{
         if(config.url.includes('/token')){
