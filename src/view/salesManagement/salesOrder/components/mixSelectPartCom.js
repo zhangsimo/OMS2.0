28b389164785f4e23ea341f939ba318c2d94e4dc
zhangsimo8 <zhangsimo8@sina.com>
2019-12-27 11:23:10 +0800

    添加配置文件

--- src/view/salesManagement/salesOrder/components/mixSelectPartCom.js
+++ src/view/salesManagement/salesOrder/components/mixSelectPartCom.js
@@ -225,7 +225,7 @@ export const mixSelectPartCom = {
       }
       req.page = this.page.num;
       req.size = this.page.size;
-      getwbParts(req).then(res => {
+      getwbParts({},req).then(res => {
         this.loading = false;
         this.partData = res.data.content || [];
         this.page.total = res.data.totalElements;
--- src/view/salesManagement/salesOrder/components/mixSupplier.js
+++ src/view/salesManagement/salesOrder/components/mixSupplier.js
@@ -215,7 +215,7 @@ export const mixSelectPartCom = {
       }
       req.page = this.page.num
       req.size = this.page.size
-      getwbParts(req).then(res => {
+      getwbParts({},req).then(res => {
         this.loading = false;
         this.partData = res.data.content || [];
         this.page.total = res.data.totalElements
