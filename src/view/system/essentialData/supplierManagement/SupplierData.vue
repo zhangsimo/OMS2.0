6b494c7307c248407256f46968775471b294d8c6
yujiayan <yujiayan@g-part.cn>
2019-12-30 21:12:28 +0800

    供应商管理

--- src/view/system/essentialData/supplierManagement/SupplierData.vue
+++ src/view/system/essentialData/supplierManagement/SupplierData.vue
@@ -426,6 +426,7 @@ export default {
     // 上传成功函数
     onSuccess(response) {
       this.getlist();
+      console.log(response)
       if (response.code != 0) {
         this.$Notice.warning({
           title: "导入失败",
