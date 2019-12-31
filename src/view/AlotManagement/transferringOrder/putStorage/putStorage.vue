36abc284e2161f80572aeaaddfae5efe53b48031
Mrhe111 <18257377846m@sina.cn>
2019-12-31 10:12:38 +0800

    提交

--- src/view/AlotManagement/transferringOrder/putStorage/putStorage.vue
+++ src/view/AlotManagement/transferringOrder/putStorage/putStorage.vue
@@ -163,12 +163,12 @@
                 <div v-show="staaa" class="flex plan-cz-btn" ref="planBtn">
                   <div class="clearfix">
                     <div v-show="staaa" class="fl mb5">
-                      <Button size="small" class="mr10" @click="addProoo">
+                      <Button v-has="'addProoo'" size="small" class="mr10" @click="addProoo">
                         <Icon type="md-add" />选择调拨入库单
                       </Button>
                     </div>
                     <div v-show="staaa" class="fl mb5">
-                      <Button size="small" class="mr10" @click="shanchu">
+                      <Button v-has="'delete'" size="small" class="mr10" @click="shanchu">
                         <i class="iconfont mr5 iconlajitongicon"></i> 删除配件
                       </Button>
                     </div>
