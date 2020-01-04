d755f70f7872c6c1127eaaa373bdf2ca1d6a60c1
yujiayan <yujiayan@g-part.cn>
2020-01-04 16:18:36 +0800

    结算

--- src/view/settlementManagement/bill/components/selectCompany.vue
+++ src/view/settlementManagement/bill/components/selectCompany.vue
@@ -2,9 +2,9 @@
   <div>
     <Modal v-model="searchPartLayer" title="供应商" width="1000">
       <div class="partCheck-hd">
-        <Input class="w150 mr10" v-model="fullName" placeholder="名称"></Input>
-        <Input class="w200 mr10" v-model="code" placeholder="编码"></Input>
-        <Input class="w150 mr10" v-model="contactorTel" placeholder="电话"></Input>
+        <Input class="w150 mr10" v-model="fullName" placeholder="名称"/>
+        <Input class="w200 mr10" v-model="code" placeholder="编码"/>
+        <Input class="w150 mr10" v-model="contactorTel" placeholder="电话"/>
         │
         <Checkbox class="mr20 ml10" v-model="isDisable"> 显示禁用</Checkbox>
         <Button @click="search" class="mr10" type='primary'><Icon type="ios-search" size="14" /> 查询</Button>
