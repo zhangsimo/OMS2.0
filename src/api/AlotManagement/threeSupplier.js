105f7b43ad8378298c25a6bd5264086f623ebfac
Mrhe111 <18257377846m@sina.cn>
2020-01-06 18:08:43 +0800

    供应商直发入库-报500错误

--- src/api/AlotManagement/threeSupplier.js
+++ src/api/AlotManagement/threeSupplier.js
@@ -1,25 +1,25 @@
-import api from '_conf/url';
-import axios from '@/libs/api.request';
+import api from "_conf/url";
+import axios from "@/libs/api.request";
 //获取仓库
 export function getcangku() {
   return axios.request({
     url: `${api.wmsApi}/comStore/stores/queryByOrgid`,
-    method: 'get'
+    method: "get"
   });
 }
 //获取客户
 export function getbayer(params) {
   return axios.request({
     url: `${api.wmsApi}/guest/queryGuestList`,
-    method: 'get',
+    method: "get",
     params
   });
 }
 // 供应商直调列表
 export function zongbuzhidiaoList(page, size, data) {
   return axios.request({
-    url: `${api.omsOrder}/pchsOrderMain/queryByConditions?page=${page}&size=${size}`,
-    method: 'post',
+    url: `${api.omsOrder}/pchsOrderMain/queryDirectByConditions?page=${page}&size=${size}`,
+    method: "post",
     data
   });
 }
@@ -28,7 +28,7 @@ export function zongbuzhidiaoList(page, size, data) {
 export function ListDetail(data) {
   return axios.request({
     url: `${api.omsOrder}/pchsPlanMain/guestClassify`,
-    method: 'post',
+    method: "post",
     data
   });
 }
@@ -37,7 +37,7 @@ export function ListDetail(data) {
 export function daohuoruku(data) {
   return axios.request({
     url: `${api.omsOrder}/pchsOrderMain/arrival`,
-    method: 'post',
+    method: "post",
     data
   });
 }
