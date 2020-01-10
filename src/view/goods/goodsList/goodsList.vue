d0f1e0a6ac8c5b2d87d183da8d6d1e1626261314
bitcat <1653399637@qq.com>
2020-01-10 18:59:47 +0800

    bugfix

--- src/view/goods/goodsList/goodsList.vue
+++ src/view/goods/goodsList/goodsList.vue
@@ -30,7 +30,6 @@
               @click="submit(1)"
               v-has="'save'"
               class="mr10"
-              :loading="submitloading"
             >
               <i class="iconfont mr5 iconbaocunicon"></i>保存
             </Button>
@@ -41,7 +40,6 @@
               :disabled="![0, 4].includes(selectPlanOrderItem.billStatusId)"
               @click="submit(2)"
               v-has="'submit'"
-              :loading="submitloading"
             >
               <i class="iconfont mr5 iconziyuan2"></i>提交
             </Button>
