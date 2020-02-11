86cba110161425ae7cb33efb7ee5e493dd45af8a
zhangsimo8 <zhangsimo8@sina.com>
2020-02-11 18:47:47 +0800

    添加配置文件

--- src/view/salesManagement/salesOrder/Order/OrderRight.vue
+++ src/view/salesManagement/salesOrder/Order/OrderRight.vue
@@ -345,7 +345,7 @@
     <!--      </Modal>-->
 
     <!--      添加配件-->
-    <select-part-com ref="selectPartCom" @selectPartName="getPartNameList"></select-part-com>
+    <select-part-com ref="selectPartCom" :guestId="formPlan.guestId" @selectPartName="getPartNameList"></select-part-com>
     <!--      批次配件-->
     <barch ref="barch" @selectPartName="getBarchList"></barch>
     <!--      选择客户-->
