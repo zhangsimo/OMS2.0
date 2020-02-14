753add52eafadb883d554a175dee6f6e5f5c0081
zhangluqiang1 <zhangluqiang@g-part.cn>
2020-02-14 17:35:00 +0800

    临时采购弹窗

--- src/view/goods/goodsList/components/mixSelectPartCom.js
+++ src/view/goods/goodsList/components/mixSelectPartCom.js
@@ -253,7 +253,7 @@ export const mixSelectPartCom = {
       // console.log(this.selectTableItem);
       if (this.selectTableItem.length > 0) {
         this.$emit("selectPartName", this.selectTableItem);
-        // this.searchPartLayer = false;
+        this.searchPartLayer = false;
       } else {
         this.$Message.error("请选择数据");
       }
