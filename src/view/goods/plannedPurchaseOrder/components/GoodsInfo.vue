58f2b28106cd5ab273e0708228983f14905485fe
zapeng <526744312@qq.com>
2020-03-16 15:10:19 +0800

    修复bug

--- src/view/goods/plannedPurchaseOrder/components/GoodsInfo.vue
+++ src/view/goods/plannedPurchaseOrder/components/GoodsInfo.vue
@@ -290,9 +290,10 @@ export default class GoodsInfo extends Vue {
   }
 
   private async getLists() {
+    const parentD:any = this.$parent
     this.showInfo = true;
     const directCompanyId = this.row.directCompanyId || null;
-    let res:any = await fapi.getGoodsInfos({ mainId: this.mainId, directCompanyId });
+    let res:any = await fapi.getGoodsInfos({ mainId: this.mainId,guestId:parentD.formPlanmain.guestId, directCompanyId });
     if (res.code == 0) {
       this.tableData = res.data;
       this.loading = false;
@@ -311,7 +312,8 @@ export default class GoodsInfo extends Vue {
   }
   //获取物流下拉框
   private async inlogistics() {
-    let params:any = {}
+    const parentD:any = this.$parent
+    let params:any = {guestId:parentD.formPlanmain.guestId}
     if(this.formDateRight.deliveryType == 2){
       params.logisticsType = '020701'
     }
