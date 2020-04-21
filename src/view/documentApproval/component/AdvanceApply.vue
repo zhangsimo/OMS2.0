51fe76f45334a0ecff7fc4baca566268a9f4972c
zhangsimo <zhangsimo@g-part.cn>
2020-04-21 16:45:39 +0800

    少组件

--- src/view/documentApproval/component/AdvanceApply.vue
+++ src/view/documentApproval/component/AdvanceApply.vue
@@ -51,7 +51,7 @@
         </div>
 
 
-        <h5 class="mt20 mb10" style="font-size: 18px">其他应付款单据</h5>
+        <h5 class="mt20 mb10" style="font-size: 18px">预付款采购单据</h5>
         <Button class="mb10" @click="SelectTheDocuments">选择单据</Button>
         <vxe-table
           class="mt10"
