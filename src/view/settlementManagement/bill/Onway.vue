cb413e8244fdea18191f2a7a4c0432e3a8c4a4b6
zhouweijian <zhouweijian@g-part.cn>
2020-04-28 10:36:33 +0800

    修改结束时间传值问题

--- src/view/settlementManagement/bill/Onway.vue
+++ src/view/settlementManagement/bill/Onway.vue
@@ -310,7 +310,7 @@ export default {
     getGeneral() {
       let data={
         startTime:this.value[0] ? moment(this.value[0]).format("YYYY-MM-DD HH:mm:ss") : '',
-        endTime:this.value[1] ? moment(this.value[1]).format("YYYY-MM-DD HH:mm:ss") : '',
+        endTime:this.value[1] ? moment(this.value[1]).format("YYYY-MM-DD HH:mm:ss").split(' ')[0]+" 23:59:59" : '',
         orgid:this.model1,
         code:this.fno,
         guestId:this.companyId
