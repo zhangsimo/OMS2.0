b2e7220a95f31647ac8ee26860103220a96d9abc
俞佳艳 <1065894111@qq.com>
2020-04-30 15:37:10 +0800

    调拨出库

--- src/view/AlotManagement/transferringOrder/stockRemoval/stockRemoval.vue
+++ src/view/AlotManagement/transferringOrder/stockRemoval/stockRemoval.vue
@@ -1140,12 +1140,13 @@ export default {
         }, 1000);
 
         seleList.map(item => {
-          arr.push(item.id);
+          if(item.id)arr.push(item.id);
         });
         const params = {
           ids: arr,
           mainId: this.Leftcurrentrow.id
         };
+        if(params.ids.length===0) return
         shanqu(params)
           .then(res => {
             // 导入成品, 并把成品覆盖掉当前配件组装信息list
