ad826fa7bac5e7d70d1cc37e4887ebb80870cedd
zhangsimo <zhangsimo@g-part.cn>
2020-05-21 18:01:28 +0800

    少组件

--- config/env.js
+++ config/env.js
@@ -9,3 +9,19 @@ export default "test"
 
 // 生产
 // export default 'pro'
+let output = 'test' //本地修改环境
+const origin = location.hostname
+if (origin.includes('devoms2')){
+  output = 'dev'
+}
+if (origin.includes('tsoms2')){
+  output = 'test'
+}
+if (origin.includes('uatoms2')){
+  output = 'audit'
+}
+if (origin.includes('erp')){
+  output = 'pro'
+}
+console.log(output , '测试当前环境')
+// export default output
