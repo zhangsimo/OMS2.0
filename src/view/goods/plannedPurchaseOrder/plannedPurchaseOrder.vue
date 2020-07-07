a679c7404e327134e4f3244920fc60fb6c6096bf
zhangluqiang1 <zhangluqiang@g-part.cn>
2020-07-07 17:07:49 +0800

    2755111

--- src/view/goods/plannedPurchaseOrder/plannedPurchaseOrder.vue
+++ src/view/goods/plannedPurchaseOrder/plannedPurchaseOrder.vue
@@ -299,7 +299,7 @@
                       @click="selectPlan"
                       :disabled="isInput || formPlanmain.guestId.length <= 0"
                       v-has="'selectProject'"
-                      >选择采购计划</Button
+                      >选择滚动计划</Button
                     >
                   </div>
                   <div class="fl mb5">
