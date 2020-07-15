0394f5892f1ced637c2a2c805926822e3dc6931a
bitcat <1653399637@qq.com>
2020-07-15 09:52:40 +0800

    测试

--- src/view/documentApproval/component/index/AdvanceApply.js
+++ src/view/documentApproval/component/index/AdvanceApply.js
@@ -1,182 +1,209 @@
-import  moment from 'moment'
-import selectAdvanceApply from '../popWindow/SelectAdvanceApply'
-import upphoto from '../Upphoto'
-import flowbox from '../Flow'
-import {getAdvanceSave} from '_api/documentApproval/AdvanceApply.js'
-import { getThisAllList } from '@/api/documentApproval/documentApproval/documentApproval'
-import {getAccountName} from "../../../../api/bill/saleOrder";
-import {getPost} from "../utils";
+import moment from "moment";
+import selectAdvanceApply from "../popWindow/SelectAdvanceApply";
+import upphoto from "../Upphoto";
+import flowbox from "../Flow";
+import { getAdvanceSave } from "_api/documentApproval/AdvanceApply.js";
+import { getThisAllList } from "@/api/documentApproval/documentApproval/documentApproval";
+import { getAccountName } from "../../../../api/bill/saleOrder";
+import { getPost } from "../utils";
 
 export default {
   name: "AdvanceApply",
-  components:{
+  components: {
     selectAdvanceApply,
     upphoto,
     flowbox
   },
-  props:{
-    list:''
+  props: {
+    list: ""
   },
-  data(){
+  data() {
     return {
+      remoteloading: false,
       model: false, //模态框开关
       modelType: false, //模态框打开模式 0-新增 1-编辑 3-查看
-      formInline:{
-      },//所有数据对象
+      formInline: {}, //所有数据对象
       //表单校验
       ruleValidate: {
-        topic: [
-          {required: true, message: '主题为必填', trigger: 'blur'}
+        topic: [{ required: true, message: "主题为必填", trigger: "blur" }],
+        applyAmt: [
+          { required: true, message: "金额为必填", trigger: "blur" },
+          {
+            pattern: /^(([1-9]{1}\d*)|(0{1}))(\.\d{1,2})?$/,
+            message: "最多保留2为小数"
+          }
         ],
-        applyAmt:[
-          {required: true, message: '金额为必填', trigger: 'blur'},
-          {pattern:/^(([1-9]{1}\d*)|(0{1}))(\.\d{1,2})?$/ , message:'最多保留2为小数'}
+        receiveGuestId: [
+          { required: true, message: "往来单位", trigger: "change" }
         ],
-        receiveGuestId:[
-          {required: true, message: '往来单位', trigger: 'change'}
+        receiver: [
+          { required: true, message: "收款人账户必填", trigger: "change" }
         ],
-        receiver:[
-          {required: true, message: '收款人账户必填', trigger: 'change'}
+        receiveBank: [
+          { required: true, message: "开户行名称必填", trigger: "blur" }
         ],
-        receiveBank:[
-          {required: true, message: '开户行名称必填', trigger: 'blur'}
+        paymentTerm: [
+          {
+            required: true,
+            type: "date",
+            message: "付款期限必填",
+            trigger: "change"
+          }
         ],
-        paymentTerm:[
-          {required: true,type: 'date',  message: '付款期限必填', trigger: 'change'}
+        receiveBankNo: [
+          { required: true, message: "开户账号必填", trigger: "blur" }
         ],
-        receiveBankNo:[
-          {required: true, message: '开户账号必填', trigger: 'blur'}
-
-        ],
-        paymentAccount:[
-          {required: true, type:'string', message: '付款账户必选', trigger: 'change'}
+        paymentAccount: [
+          {
+            required: true,
+            type: "string",
+            message: "付款账户必选",
+            trigger: "change"
+          }
         ]
         // BankNo:[
         //   {required: true, message: '银行账号必填', trigger: 'blur'}
         // ]
       },
       //费用支出表格的数据校验
-      documentTableData:[],//借支核销表格数据
-      payeeList:[],//收款人列表
-      payUserList:[],//付款人列表
-      company:[],//往来单位
-      Pictures:{},//请求回来的图片地址状态
+      documentTableData: [], //借支核销表格数据
+      payeeList: [], //收款人列表
+      payUserList: [], //付款人列表
+      company: [], //往来单位
+      options: [],
+      Pictures: {}, //请求回来的图片地址状态
 
       //收款账号
-      receiverArr:[]
-    }
+      receiverArr: []
+    };
   },
-  mounted(){
-  },
+  mounted() {},
 
-  methods:{
+  methods: {
     //模态框打开111
-    open(){
-      this.company = this.list.salesList
-      this.payUserList = this.list.payList
-      this.formInline = {}
-      this.$refs.upImg.uploadListModal = []
-      this.$refs.upImg.uploadList = []
-      this.$refs['formInline'].resetFields();
-      this.model = true
+    open() {
+      this.company = [];
+      this.options = this.list.salesList;
+      this.payUserList = this.list.payList;
+      this.formInline = {};
+      this.$refs.upImg.uploadListModal = [];
+      this.$refs.upImg.uploadList = [];
+      this.$refs["formInline"].resetFields();
+      this.model = true;
       //判断模态框状态
-      this.modelType = false
+      this.modelType = false;
 
       if (this.list.type == 1) {
         let date = moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
-          user = this.$store.state.user.userData
-        this.formInline.applicant = user.staffName
-        this.formInline.deptName = user.groups.length > 0 ?  user.groups[user.groups.length - 1].name :''
-        this.formInline.shopCode = user.shopCode || ' 　　'
+          user = this.$store.state.user.userData;
+        this.formInline.applicant = user.staffName;
+        this.formInline.deptName =
+          user.groups.length > 0
+            ? user.groups[user.groups.length - 1].name
+            : "";
+        this.formInline.shopCode = user.shopCode || " 　　";
         this.formInline.orgName = getPost();
-        this.formInline.applyTypeName = '采购预付款'
-        this.formInline.applyTime = date
+        this.formInline.applyTypeName = "采购预付款";
+        this.formInline.applyTime = date;
         this.formInline.paymentOrgName = getPost();
       }
-      if (this.list.type == 2){
-        this.getList()
+      if (this.list.type == 2) {
+        this.getList();
       }
-      if (this.list.type == 3 || this.list.type == 4){
-        this.getList()
-        this.modelType = true
+      if (this.list.type == 3 || this.list.type == 4) {
+        this.getList();
+        this.modelType = true;
       }
     },
 
     //获取当前信息
-    async getList(){
-      let data ={}
-      data.id = this.list.id || ''
-      let res = await getThisAllList(data)
-      if(res.code === 0){
-        this.$nextTick( () => {
-          this.formInline = res.data
+    async getList() {
+      let data = {};
+      data.id = this.list.id || "";
+      let res = await getThisAllList(data);
+      if (res.code === 0) {
+        this.$nextTick(() => {
+          this.formInline = res.data;
           this.Pictures = {
-            voucherPictures :res.data.voucherPictures,
+            voucherPictures: res.data.voucherPictures,
             billStatus: res.data.billStatus
-          }
-        })
-
+          };
+        });
       }
     },
 
+    remoteMethod(query) {
+      this.company = [];
+      if (query !== "") {
+        this.remoteloading = true;
+        this.company = [];
+        const list = this.options.map(item => {
+          return {
+            value: item.value,
+            label: item.label
+          };
+        });
+        this.company = list.filter(
+          item => item.label.toLowerCase().indexOf(query.toLowerCase()) > -1
+        );
+        this.remoteloading = false;
+      } else {
+        this.company = [];
+      }
+    },
 
     //获取往来单位
     getCompany(row) {
       // let arr = this.company.filter( item => item.value == row.value)
-      this.getAccountNameList(row)
+      this.getAccountNameList(row);
     },
 
-
     //获取收款户名
-    async getAccountNameList(row){
-      let rep = await getAccountName({"guestId":row.value});
-      if(rep.code==0){
+    async getAccountNameList(row) {
+      let rep = await getAccountName({ guestId: row.value });
+      if (rep.code == 0) {
         this.receiverArr = rep.data;
-        if(rep.data.length==1){
-          this.setReceiverInfo(this.receiverArr[0])
-        }else{
-          this.formInline.receiver = ''
-          this.formInline.receiveBank = ''
-          this.formInline.receiveBankNo = '';
+        if (rep.data.length == 1) {
+          this.setReceiverInfo(this.receiverArr[0]);
+        } else {
+          this.formInline.receiver = "";
+          this.formInline.receiveBank = "";
+          this.formInline.receiveBankNo = "";
         }
       }
     },
 
-    setReceiverInfo(row){
+    setReceiverInfo(row) {
       this.formInline.receiver = row.id;
       this.formInline.receiveBank = row.accountBank;
       this.formInline.receiveBankNo = row.accountBankNo;
     },
 
-    changeCollectionUname(v){
-      let arr = this.receiverArr.filter(item => item.id==v);
+    changeCollectionUname(v) {
+      let arr = this.receiverArr.filter(item => item.id == v);
       this.setReceiverInfo(arr[0]);
     },
 
-
-
-
     //打开选择模态框
-    openSelect(){
-      this.$refs.request.open()
+    openSelect() {
+      this.$refs.request.open();
     },
 
-
     //删除行
-    dele(row){
+    dele(row) {
       // this.$refs.xTable.remove(this.expenditureTableData[row.seq - 1])
-      this.formInline.details.splice((row.seq - 1) , 1)
+      this.formInline.details.splice(row.seq - 1, 1);
     },
 
     //获取其他付款单据信息
-    otherPayList(row){
-      delete row.id
+    otherPayList(row) {
+      delete row.id;
       this.$set(this.formInline, "details", [row]);
     },
 
     //选择单据
-    SelectTheDocuments(){
-      this.$refs.documnets.open()
+    SelectTheDocuments() {
+      this.$refs.documnets.open();
     },
 
     //表格尾部合计
@@ -184,45 +211,43 @@ export default {
       return [
         columns.map((column, columnIndex) => {
           if (columnIndex === 0) {
-            return '合计'
+            return "合计";
           }
-          if (['payAmt'].includes(column.property)) {
-            return this.$utils.sum(data, column.property)
+          if (["payAmt"].includes(column.property)) {
+            return this.$utils.sum(data, column.property);
           }
-          return null
+          return null;
         })
-      ]
+      ];
     },
 
     //获取付款信息
-    getPayList(value){
-      if (!value) return
-      let list = this.payUserList.filter(item => item.id == value)[0]
-      this.formInline.paymentBank = list.bankName
-      this.formInline.paymentBankNo = list.accountCode
+    getPayList(value) {
+      if (!value) return;
+      let list = this.payUserList.filter(item => item.id == value)[0];
+      this.formInline.paymentBank = list.bankName;
+      this.formInline.paymentBankNo = list.accountCode;
     },
 
     //获取到上传图片地址
-    getImgList(row){
-      this.formInline.voucherPictures = row.list
+    getImgList(row) {
+      this.formInline.voucherPictures = row.list;
     },
 
     //保存提交
-    save(type){
-
-      this.$refs.formInline.validate( async (valid) => {
+    save(type) {
+      this.$refs.formInline.validate(async valid => {
         if (valid) {
-          this.formInline.step = type
-          let res = await getAdvanceSave(this.formInline)
+          this.formInline.step = type;
+          let res = await getAdvanceSave(this.formInline);
           if (res.code == 0) {
-            this.$Message.success('操作成功')
-            this.model = false
+            this.$Message.success("操作成功");
+            this.model = false;
           }
         } else {
-          this.$Message.error('带*必填');
+          this.$Message.error("带*必填");
         }
-      })
+      });
     }
-
   }
-}
+};
