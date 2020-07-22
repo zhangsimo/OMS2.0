596ee682b03204f0e16b14a9f4628df75fa22ab0
zhangsimo <zhangsimo@g-part.cn>
2020-07-22 18:59:44 +0800

    页面数据劫持

--- src/view/settlementManagement/fundsManagement/cashJournal/cashJournal.vue
+++ src/view/settlementManagement/fundsManagement/cashJournal/cashJournal.vue
@@ -294,7 +294,7 @@ import amtData from "../../components/amtData";
 
 import moment from "moment";
 export default {
-  name:'shopJournal',
+  // name:'shopJournal',
   components: {
     quickDate,
     importXLS,
