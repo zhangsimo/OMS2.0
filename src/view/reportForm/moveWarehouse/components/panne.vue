5e5a8029052ba33598d221967506f96dd11b328c
“yangkaifeng” <yangkaifeng@g-part.cn>
2020-08-08 15:24:02 +0800

    销售-调拨-移仓-盘点-加工报表，门店清除或选择全部，仓库下拉框中显示了所有租户的仓库，未根据租户门店进行过滤

--- src/view/reportForm/moveWarehouse/components/panne.vue
+++ src/view/reportForm/moveWarehouse/components/panne.vue
@@ -38,7 +38,8 @@
                 v-for="item in stores"
                 :value="item.id"
                 :key="item.id"
-                >{{ item.name }}</Option
+              >{{ item.name }}
+              </Option
               >
             </Select>
           </div>
@@ -53,7 +54,8 @@
                 v-for="item in warehouse"
                 :value="item.id"
                 :key="item.id"
-              >{{ item.name }}</Option
+              >{{ item.name }}
+              </Option
               >
             </Select>
           </div>
@@ -70,132 +72,139 @@
 </template>
 
 <script>
-import moment from "moment";
-import QuickDate from "_c/getDate/dateget";
-import more from "./more";
-import * as api from "_api/reportForm/index.js";
-import { creat } from "@/view/settlementManagement/components";
-import {v4} from "uuid";
-import Cookies from "js-cookie";
-import {TOKEN_KEY} from "../../../../libs/util";
-import Api from "_conf/url";
-export default {
-  components: { QuickDate, more },
-  props: {
-    type: {
-      default: 0,
-      type: Number,
-    }
-  },
-  data() {
-    return {
-      warehouse: [],
-      stores: [{id:"",name:"全部"}], // 门店
-      quickDates: [], // 快速日期查询
-      search: {
-        isPanne: true,
-        submitDate: [], // 提交日期
-        content: "", // 编码名称
-        storeId: "",
-        orgid: "" // 门店
+  import moment from "moment";
+  import QuickDate from "_c/getDate/dateget";
+  import more from "./more";
+  import * as api from "_api/reportForm/index.js";
+  import {creat} from "@/view/settlementManagement/components";
+  import {v4} from "uuid";
+  import Cookies from "js-cookie";
+  import {TOKEN_KEY} from "../../../../libs/util";
+  import Api from "_conf/url";
+
+  export default {
+    components: {QuickDate, more},
+    props: {
+      type: {
+        default: 0,
+        type: Number,
       }
-    };
-  },
-  computed:{
-    selectShopList(){
-      let canSelect = this.$store.state.user.userData.currentCompany.isMaster ? true : false
-      return canSelect
     },
-    placeHM(){
-      if(this.type==1){
-        return "请选择移入仓库"
-      }else {
-        return "请选择移出仓库"
+    data() {
+      return {
+        warehouse: [],
+        stores: [{id: "", name: "全部"}], // 门店
+        quickDates: [], // 快速日期查询
+        search: {
+          isPanne: true,
+          submitDate: [], // 提交日期
+          content: "", // 编码名称
+          storeId: "",
+          orgid: "" // 门店
+        }
+      };
+    },
+    computed: {
+      selectShopList() {
+        let canSelect = this.$store.state.user.userData.currentCompany.isMaster ? true : false
+        return canSelect
+      },
+      placeHM() {
+        if (this.type == 1) {
+          return "请选择移入仓库"
+        } else {
+          return "请选择移出仓库"
+        }
       }
-    }
-  },
-  async mounted() {
-    let resE = await api.getStorelist();
-    let resW = await api.getWarehouse();
-    if(resE.code == 0) {
-       let data = resE.data;
+    },
+    async mounted() {
+      let resE = await api.getStorelist();
+      let resW = await api.getWarehouse();
+      if (resE.code == 0) {
+        let data = resE.data;
         Object.keys(data).forEach(key => {
           this.stores.push({id: key, name: data[key]})
         })
-    }
-    if(resW.code == 0) {
-      this.warehouse = resW.data;
-    }
-  },
-  methods: {
-    //获取仓库
-    async getWares(orgId) {
-      let res = JSON.parse(localStorage.getItem('oms2-userList'))
-      let tenantId = res.tenantId || 0
-      let shopkeeper = res.shopkeeper || 0
-      let uuid = v4()
-      let params={tenantId:tenantId,shopId:orgId,shopkeeper:shopkeeper,uuid:uuid,scope:"oms"}
-      await this.ajaxAll.get(`${Api.wmsApi}/comStore/stores/findByShopId`,{
-        params:params,
-        headers:{
-          Authorization: "Bearer " + Cookies.get(TOKEN_KEY)
-        }
-      }).then((res2)=>{
-        if(res2.data.code === 0) {
-          this.warehouse = res2.data.data;
-        }
-      })
-    },
-    // 快速日期查询
-    async getDataQuick(v) {
-      this.quickDates = v;
-      if(v.length >= 2) {
-        let arr = await creat("", this.$store);
-        this.search.orgid = arr[1];
-        this.search.content="";this.search.storeId=""
-        this.getWares(this.search.orgid)
-        this.$emit("search", { isPanne: true, commitStartDate: v[0], commitEndDate: v[1],orgid:this.search.orgid });
-      } else {
-        this.search.content="";this.search.storeId=""
-        this.$emit("search", { isPanne: true, orgid:this.search.orgid });
+      }
+      if (resW.code == 0) {
+        this.warehouse = resW.data;
       }
     },
-    // 查询
-    query() {
-      let data = {};
-      for (let key in this.search) {
-        if (this.search[key]) {
-          if (key == "submitDate") {
-            if (this.search["submitDate"][0]) {
-              data.commitStartDate = moment(this.search["submitDate"][0]).format("YYYY-MM-DD") + " 00:00:00";
-              data.commitEndDate = moment(this.search["submitDate"][1]).format("YYYY-MM-DD") + " 23:59:59";
+    methods: {
+      //获取仓库
+      async getWares(orgId) {
+        if (orgId == undefined) {
+          this.warehouse = []
+          return
+        }
+        let res = JSON.parse(localStorage.getItem('oms2-userList'))
+        let tenantId = res.tenantId || 0
+        let shopkeeper = res.shopkeeper || 0
+        let uuid = v4()
+        let params = {tenantId: tenantId, shopId: orgId, shopkeeper: shopkeeper, uuid: uuid, scope: "oms"}
+        await this.ajaxAll.get(`${Api.wmsApi}/comStore/stores/findByShopId`, {
+          params: params,
+          headers: {
+            Authorization: "Bearer " + Cookies.get(TOKEN_KEY)
+          }
+        }).then((res2) => {
+          if (res2.data.code === 0) {
+            this.warehouse = res2.data.data;
+          }
+        })
+      },
+      // 快速日期查询
+      async getDataQuick(v) {
+        this.quickDates = v;
+        if (v.length >= 2) {
+          let arr = await creat("", this.$store);
+          this.search.orgid = arr[1];
+          this.search.content = "";
+          this.search.storeId = ""
+          this.getWares(this.search.orgid)
+          this.$emit("search", {isPanne: true, commitStartDate: v[0], commitEndDate: v[1], orgid: this.search.orgid});
+        } else {
+          this.search.content = "";
+          this.search.storeId = ""
+          this.$emit("search", {isPanne: true, orgid: this.search.orgid});
+        }
+      },
+      // 查询
+      query() {
+        let data = {};
+        for (let key in this.search) {
+          if (this.search[key]) {
+            if (key == "submitDate") {
+              if (this.search["submitDate"][0]) {
+                data.commitStartDate = moment(this.search["submitDate"][0]).format("YYYY-MM-DD") + " 00:00:00";
+                data.commitEndDate = moment(this.search["submitDate"][1]).format("YYYY-MM-DD") + " 23:59:59";
+              }
+            } else if (key == "content" && this.search.content) {
+              data.partCode = this.search.content;
+            } else {
+              data[key] = this.search[key];
             }
-          } else if (key == "content" && this.search.content) {
-            data.partCode = this.search.content;
-          } else {
-            data[key] = this.search[key];
           }
         }
-      }
-      if(this.quickDates.length >= 2 && this.quickDates[0]) {
-        data.commitStartDate = this.quickDates[0];
-        data.commitEndDate = this.quickDates[1];
-      }
-      this.$emit("search", data);
-    },
-    // 更多
-    moreOpen() {
-      this.$refs.more.init();
-    },
-    getmoreData(data) {
-      if (data != null) {
+        if (this.quickDates.length >= 2 && this.quickDates[0]) {
+          data.commitStartDate = this.quickDates[0];
+          data.commitEndDate = this.quickDates[1];
+        }
         this.$emit("search", data);
+      },
+      // 更多
+      moreOpen() {
+        this.$refs.more.init();
+      },
+      getmoreData(data) {
+        if (data != null) {
+          this.$emit("search", data);
+        }
+      },
+      // 导出
+      exportxls() {
+        this.$emit("export");
       }
-    },
-    // 导出
-    exportxls() {
-      this.$emit("export");
     }
-  }
-};
+  };
 </script>
