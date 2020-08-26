fdbd2a4355aeec61a7d911a5134301acfb6f2b03
“yangkaifeng” <yangkaifeng@g-part.cn>
2020-08-26 21:27:19 +0800

    报表模块 - 页面样式调整_前台

--- src/view/reportForm/Assembly/components/tabFour.vue
+++ src/view/reportForm/Assembly/components/tabFour.vue
@@ -1,5 +1,6 @@
 <template>
-  <section class="con-box">
+  <section>
+<!--    class="con-box"-->
     <vxe-table
       border
       align="center"
@@ -171,6 +172,7 @@
       :current="page.num"
       :total="page.total"
       :page-size="page.size"
+      :page-size-opts="pageOpts"
       @on-change="changePage"
       @on-page-size-change="changeSize"
       show-sizer
@@ -186,9 +188,10 @@ export default {
     return {
       page: {
         page: 0,
-        size: 10,
+        size: 1000,
         total: 0
       },
+      pageOpts:[1000,2000,3000,5000],
       tableDataAll: [],
       tableData: [],
       searchData:{}
--- src/view/reportForm/Assembly/components/tabOne.vue
+++ src/view/reportForm/Assembly/components/tabOne.vue
@@ -1,8 +1,9 @@
 <template>
-  <section class="con-box">
+  <section>
+<!--    class="con-box"-->
     <vxe-table
       border
-      align="center"
+      align="left"
       size="mini"
       ref="xTable"
       height="400"
@@ -171,6 +172,7 @@
       :current="page.num"
       :total="page.total"
       :page-size="page.size"
+      :page-size-opts="pageOpts"
       @on-change="changePage"
       @on-page-size-change="changeSize"
       show-sizer
@@ -187,9 +189,10 @@ export default {
     return {
       page: {
         page: 0,
-        size: 10,
+        size: 1000,
         total: 0
       },
+      pageOpts:[1000,2000,3000,5000],
       tableDataAll: [],
       tableData: [],
       searchData:{}
