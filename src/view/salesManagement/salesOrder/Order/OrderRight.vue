4b0ed81b8c3a611633b59f332bef1878548a23a8
“yangkaifeng” <yangkaifeng@g-part.cn>
2020-08-27 14:24:31 +0800

    销售订单，备注太长无法显示全

--- src/view/salesManagement/salesOrder/Order/OrderRight.vue
+++ src/view/salesManagement/salesOrder/Order/OrderRight.vue
@@ -138,7 +138,7 @@
                 v-model="formPlan.remark"
                 :disabled="draftShow != 0|| this.$parent.$parent.ispart"
               />
-              <div slot="content" style="width: 100%;white-space:normal;word-wrap:break-word;">{{formPlan.remark?formPlan.remark.trim()=='':''}}</div>
+              <div slot="content" style="width: 100%;white-space:normal;word-wrap:break-word;">{{formPlan.remark?(formPlan.remark.trim()!=''?formPlan.remark.trim():''):''}}</div>
             </Tooltip>
           </FormItem>
 
