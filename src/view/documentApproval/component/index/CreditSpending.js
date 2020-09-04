d9af4e939cbd31591598d4b8c52c40f416900e48
zapeng <526744312@qq.com>
2020-09-04 17:22:15 +0800

    add

--- src/view/documentApproval/component/index/CreditSpending.js
+++ src/view/documentApproval/component/index/CreditSpending.js
@@ -95,8 +95,8 @@ export default {
           user = this.$store.state.user.userData
         this.formInline.applicant = user.staffName
         this.formInline.deptName = user.groups.length > 0 ? user.groups[user.groups.length - 1].name : ''
-        this.formInline.shopCode = user.shopCode || ' 　　'
-        this.formInline.orgName = getPost();
+        this.formInline.shopCode = user.currentCompany?user.currentCompany.code : "";
+        this.formInline.orgName = user.currentCompany?user.currentCompany.shortName : "";
         this.formInline.applyTypeName = '预收款支出'
         this.formInline.applyTime = date
         this.formInline.paymentOrgName = getPost();
