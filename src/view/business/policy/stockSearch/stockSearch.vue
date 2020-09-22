dc006e033e3154ffd0a5bf395bc9181cf97f02bd
zapeng <526744312@qq.com>
2020-09-22 11:48:06 +0800

    add

--- src/view/business/policy/stockSearch/stockSearch.vue
+++ src/view/business/policy/stockSearch/stockSearch.vue
@@ -35,7 +35,7 @@
           <div class="pt10">
             <Input
               v-model="searchForm.partCode"
-              placeholder="编码"
+              placeholder="品牌编码"
               class="w100 mr10"
               @on-enter="serch"
             />
@@ -193,7 +193,7 @@
             <!--<Input v-model="searchForm1.partCode" placeholder="配件编码" class="w200 mr10"></Input>-->
             <Input
               v-model="searchForm1.partCode"
-              placeholder="编码"
+              placeholder="品牌编码"
               class="w100 mr10"
               @on-enter="queryBatch"
             ></Input>
