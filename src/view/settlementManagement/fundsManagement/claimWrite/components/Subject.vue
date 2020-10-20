da08536b2ad5c5c62fd3ddf7ece88790c0c9856f
“yangkaifeng” <yangkaifeng@g-part.cn>
2020-10-20 17:29:03 +0800

    辅助核算=》不管是客户还是供应商=》auxiliaryTypeCode都传1

--- src/view/settlementManagement/fundsManagement/claimWrite/components/Subject.vue
+++ src/view/settlementManagement/fundsManagement/claimWrite/components/Subject.vue
@@ -146,7 +146,7 @@ export default {
       data.claimType=8;
       data.claimMoney = this.clime[0].unClaimedAmt
       if(this.oneSubject.auxiliaryAccountingName){
-        data.auxiliaryTypeCode=this.$refs.voucherInput.auxiliaryTypeCode //辅助核算选中哪一个
+        data.auxiliaryTypeCode = this.$refs.voucherInput.auxiliaryTypeCode == 2?1:this.$refs.voucherInput.auxiliaryTypeCode //辅助核算选中哪一个
         if(data.auxiliaryTypeCode=="1" || data.auxiliaryTypeCode=="2" || data.auxiliaryTypeCode=="3" || data.auxiliaryTypeCode=="4"){
           data.isAuxiliaryAccounting=0 //是否辅助核算类
         }else{
