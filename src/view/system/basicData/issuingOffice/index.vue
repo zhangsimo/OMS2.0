aa86d73e81326481a6558526615519870eb81d66
zapeng <526744312@qq.com>
2020-10-21 11:47:41 +0800

    add

--- src/view/system/basicData/issuingOffice/index.vue
+++ src/view/system/basicData/issuingOffice/index.vue
@@ -6,21 +6,21 @@
         <div class="wlf">
           <div class="db mr20">
             <span class="mr10">门店</span>
-            <Select v-model="shopId" style="width:150px" @on-change='changeShop'>
-              <Option v-for="item in shopListArr" :value="item.id" :key="item.id">{{ item.name }}</Option>
+            <Select v-model="shopId" style="width:250px" @on-change='changeShop'>
+              <Option v-for="item in shopListArr" :value="item.id" :key="item.id">{{ item.shortName }}</Option>
             </Select>
           </div>
           <div class="db">
             <span class="mr10">开票单位</span>
-            <Input class="w160 mr20"/>
+            <Input class="w160 mr20" v-model="name"/>
           </div>
           <div class="db">
-            <Button class="mr10" v-has="'add'">
-              <Icon type="md-add" />查询
+            <Button class="mr10" @click="getList">
+              查询
             </Button>
           </div>
           <div class="db">
-            <Button class="mr10" @click="addIssuing" v-has="'add'">
+            <Button class="mr10" @click="addIssuing">
               <Icon type="md-add" />新增
             </Button>
           </div>
@@ -110,9 +110,11 @@
             show-overflow="tooltip"
             field="unsalableQty"
             title="可见门店"
-            width="100"
+            min-width="200"
           >
-
+            <template v-slot="{row}">
+              <tag v-for="v in row.shopNameList">{{v.shopName}}</tag>
+            </template>
           </vxe-table-column>
         </vxe-table>
       </div>
@@ -125,39 +127,40 @@
     >
       <Form ref="proModalForm" :model="formValidate" :rules="ruleValidate" :label-width="110">
         <Row>
-          <!--<Col span="11">-->
-            <!--<FormItem label="所属区域：" prop="qualityTypeId">-->
-              <!--<Select v-model="formValidate.areaId" style="width:200px" label-in-value>-->
-                <!--<Option v-for="item in Branchstore" :value="item.id" :key="item.id">{{ item.companyName }}</Option>-->
-              <!--</Select>-->
-            <!--</FormItem>-->
-          <!--</Col>-->
           <Col span="11">
-            <FormItem label="所属门店：" prop="partBrandId">
-              <Select v-model="formValidate.shopNumber" style="width:200px"  label-in-value>
-                <Option v-for="item in shopListArr" :value="item.id" :key="item.id">{{ item.name }}</Option>
+            <FormItem label="所属区域：" prop="areaId">
+              <Select v-model="formValidate.areaId" style="width:190px" label-in-value>
+                <Option v-if="item.id!=='0'" v-for="item in Branchstore" :value="item.id" :key="item.id">{{ item.companyName }}</Option>
               </Select>
             </FormItem>
           </Col>
           <Col span="11">
-            <FormItem label="所属店号：" prop="code">
-              <Input v-model="formValidate.code"></Input>
+            <FormItem label="所属门店：" prop="orgid">
+              <Select v-model="formValidate.orgid" style="width:190px" @on-change="changeShopIssuing">
+                <Option v-if="item.id!=='0'" v-for="item in shopListArr" :value="item.id" :key="item.id">{{ item.name }}</Option>
+              </Select>
             </FormItem>
           </Col>
         </Row>
         <Row>
           <Col span="11">
-            <FormItem label="发票单位：" prop="name">
-              <Input
-                icon="ios-more"
-                v-model="formValidate.name"
-                readonly="readonly"
-              ></Input>
+            <FormItem label="所属店号：" prop="shopCode">
+              <Input v-model="formValidate.shopCode" style="width:190px" disabled></Input>
             </FormItem>
           </Col>
           <Col span="11">
-            <FormItem label="发票种类：" prop="partTypeF">
-              <Select v-model="formValidate.partTypeF" filterable>
+            <FormItem label="发票单位：" prop="name">
+              <Input
+                v-model="formValidate.name"
+                style="width:190px"
+              ></Input>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="11">
+            <FormItem label="发票种类：" prop="invoiceTypeCode">
+              <Select v-model="formValidate.invoiceTypeCode" style="width:190px" filterable>
                 <Option
                   v-for="item in invoiceMap"
                   :value="item.itemCode"
@@ -166,24 +169,11 @@
               </Select>
             </FormItem>
           </Col>
-        </Row>
-        <Row>
           <Col span="11">
-            <FormItem label="发票版本：" prop="partTypeS">
-              <Select v-model="formValidate.version" filterable>
+            <FormItem label="发票版本：" prop="invoiceVersion">
+              <Select v-model="formValidate.invoiceVersion" style="width:190px" filterable>
                 <Option
                   v-for="item in InvoiceVersion"
-                  :value="item.value"
-                  :key="item.value"
-                >{{item.label}}</Option>
-              </Select>
-            </FormItem>
-          </Col>
-          <Col span="11">
-            <FormItem label="适用税率：" prop="partTypeF">
-              <Select v-model="formValidate.rate" filterable>
-                <Option
-                  v-for="item in taxRate"
                   :value="item.itemValueOne"
                   :key="item.itemValueOne"
                 >{{item.itemName}}</Option>
@@ -193,15 +183,26 @@
         </Row>
         <Row>
           <Col span="11">
-            <FormItem label="开票限额：" prop="partTypeS">
-              <Input :value="((formValidate.version||0)*(1+(formValidate.rate)||0)).toFixed(0)"></Input>
+            <FormItem label="适用税率：" prop="taxRate">
+              <Select v-model="formValidate.taxRate" style="width:190px" filterable>
+                <Option
+                  v-for="item in taxRate"
+                  :value="item.itemValueOne"
+                  :key="item.itemValueOne"
+                >{{item.itemName}}</Option>
+              </Select>
+            </FormItem>
+          </Col>
+          <Col span="11">
+            <FormItem label="开票限额：">
+              <Input disabled style="width:190px" :value="issuingXE"></Input>
             </FormItem>
           </Col>
         </Row>
         <Row>
           <Col span="22">
-            <FormItem label="可见门店：" prop="partTypeF">
-              <Input class="w200" :value="formValidate.shopListName"></Input>
+            <FormItem label="可见门店：" prop="strShopList">
+              <Input class="w350" v-model="formValidate.strShopList"></Input>
               <Button class="ml10" @click="openStoreModal">选择</Button>
             </FormItem>
           </Col>
@@ -228,7 +229,9 @@
 		  return {
         loading:true,
 		    //查询值
-        shopId:'',
+        shopId:this.$store.state.user.userData.currentShopId ||"",
+        //开票单位
+        name:'',
 		    //开票信息层
         issuingInfo:false,
         //开票信息数据字段
@@ -236,35 +239,30 @@
           shopListName:'111'
         },
 
-        ruleValidate:{},
+        ruleValidate:{
+
+        },
 
         Branchstore: [{id: "0", companyName: '全部'}], //区域数组
         shopListArr: [{id: "0", name: '全部'}], //门店数组
         taxRate:[],//税率
         invoiceMap:[],//发票
         //发票版本
-        InvoiceVersion:[
-          {
-            value:"10000",
-            label:"一万限额"
-          },
-          {
-            value:"100000",
-            label:"十万限额"
-          },
-          {
-            value:"1000000",
-            label:"百万限额"
-          }
-        ],
+        InvoiceVersion:[],
         //可见门店id
         shopList:[],
         tableData:[],
       }
     },
+    computed:{
+		  issuingXE(){
+        return ((this.formValidate.version||0)*(1+(this.formValidate.rate)||0)).toFixed(0)
+      },
+    },
     mounted(){
 		  this.getAllAre();
 		  this.getShop();
+      this.getList();
     },
     methods:{
       //获取全部地址
@@ -283,14 +281,16 @@
         this.shopListArr = [{id: "0", name: '全部'}]
         let res = await goshop(data)
         if (res.code === 0) {
-          this.shopListArr = [...this.shopListArr, ...res.data]
-          this.shopId = this.$store.state.user.userData.currentShopId ||"";
-          this.getList();
+          this.shopListArr = [...this.shopListArr, ...res.data];
         }
       },
 
-      addIssuing(){
+      addIssuing(row){
         this.issuingInfo = true;
+        if(row){
+
+          // this.formValidate.shopNumber =
+        }
       },
 
       //显示门店弹框
@@ -316,15 +316,15 @@
       async getRate() {
         let data = {};
         //107票据类型
-        data = ["CS00107", "FYLX", "TaxRate"];
+        data = ["FPZL", "TaxRate","FPBB"];
         let res = await getDigitalDictionary(data);
         if (res.code == 0) {
-          //费用类型
-          this.moneyTypeList = res.data["FYLX"] || [];
           //税率
           this.taxRate = res.data["TaxRate"] || [];
           //票据类型
-          this.invoiceMap = res.data["CS00107"] || [];
+          this.invoiceMap = res.data["FPZL"] || [];
+          //发票版本
+          this.InvoiceVersion = res.data["FPBB"]||[];
         }
       },
 
@@ -332,9 +332,23 @@
         this.getList();
       },
 
+      //开票信息层切换门店
+      changeShopIssuing(v){
+        let filterData = this.shopListArr.filter(item => item.id==v);
+        if(filterData.length>0){
+          this.formValidate.code = filterData[0].code;
+        }
+      },
+
       async getList(){
         this.loading = true;
-        let rep = await getIssuingList({orgid:this.shopId});
+        //请求参数
+        let req = {};
+        if(this.name.trim()){
+          req.name = this.name
+        }
+        req.orgid = this.shopId;
+        const rep = await getIssuingList(req);
         this.loading = false;
         if(rep.code==0){
           this.tableData = rep.data['content']||[];
