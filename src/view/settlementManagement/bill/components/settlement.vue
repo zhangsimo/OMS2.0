41029251e9d9923816a2d18157f40aaddef8f38d
“yangkaifeng” <yangkaifeng@g-part.cn>
2020-12-01 21:12:46 +0800

    核销金额=》校验后输入正确金额bugbugbug

--- src/view/settlementManagement/bill/components/settlement.vue
+++ src/view/settlementManagement/bill/components/settlement.vue
@@ -433,7 +433,7 @@
           let sumUnAmt = row.unAmt;
           this.$refs.xTable.updateFooter();
           this.checkComputed();
-          if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0) || (sumUnAmt < row.rpAmt)) {
+          if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0) || (Math.abs(sumUnAmt) < Math.abs(row.rpAmt))) {
             return this.$Message.error("金额录入错误，请重新录入！")
           }
         }
@@ -467,7 +467,7 @@
               let sumUnAmt = row.unAmt;
               this.$refs.xTable.updateFooter();
               this.checkComputed();
-              if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0)||(sumUnAmt < row.rpAmt)) {
+              if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0)|| (Math.abs(sumUnAmt) < Math.abs(row.rpAmt))) {
                 this.$Message.error("金额录入错误，请重新录入！")
                 bool = false
                 return
--- src/view/settlementManagement/otherPayable/components/settlement.vue
+++ src/view/settlementManagement/otherPayable/components/settlement.vue
@@ -414,7 +414,7 @@
           let sumUnAmt = row.unAmt
           this.$refs.xTree.updateFooter();
           this.checkComputed();
-          if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0) || (sumUnAmt < row.rpAmt)) {
+          if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0) || (Math.abs(sumUnAmt) < Math.abs(row.rpAmt))) {
             return this.$Message.error("金额录入错误，请重新录入！")
           }
         }
@@ -455,7 +455,7 @@
                   let sumUnAmt = row.unAmt;
                   this.$refs.xTable.updateFooter();
                   this.checkComputed();
-                  if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0) || (sumUnAmt < row.rpAmt)) {
+                  if ((sumUnAmt > 0 && row.rpAmt <= 0) || (sumUnAmt < 0 && row.rpAmt >= 0) || (Math.abs(sumUnAmt) < Math.abs(row.rpAmt))) {
                     this.$Message.error("金额录入错误，请重新录入！")
                     bool = false
                     return
