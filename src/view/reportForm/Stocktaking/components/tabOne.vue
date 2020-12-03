be1b60ee9201b626e28f1bf80b9e75221479783b
“yangkaifeng” <yangkaifeng@g-part.cn>
2020-12-03 17:11:46 +0800

    报表统计=》盘盈入库盘亏出库=》分页没有给方法里面传递参数

--- src/view/reportForm/Stocktaking/components/tabOne.vue
+++ src/view/reportForm/Stocktaking/components/tabOne.vue
@@ -228,12 +228,12 @@ export default {
     //分页
     changePage(p) {
       this.page.page = p-1;
-      this.getList();
+      this.getList(this.searchData);
     },
     changeSize(size) {
       this.page.page = 0;
       this.page.size = size;
-      this.getList();
+      this.getList(this.searchData);
     },
     //表尾合计
     footerMethod({ columns, data }) {
