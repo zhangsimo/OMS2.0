31f5fafa1506f2cea94fcd8d6b89e6d11665fbc1
“yangkaifeng” <yangkaifeng@g-part.cn>
2020-12-16 17:38:20 +0800

    111

--- src/view/afterSale/claimSheet/claimSupplier/components/panne.vue
+++ src/view/afterSale/claimSheet/claimSupplier/components/panne.vue
@@ -1,72 +1,66 @@
 <template>
-  <section class="oper-box">
-    <div class="flex">
-      <div class="con-box flex">
-        <div class="wlf">
+  <section class="oper-top pb15">
+    <div class="oper-top">
+      <div class="oper-top flex">
+        <div class="wlf ">
           <div class="db mr10">
-            <span>提交日期：</span>
+            <span class="mr10">提交日期：</span>
             <DatePicker
-              @on-change="getDataQuick2"
+              @on-change="getDataQuick"
               :value="search.orderDate"
               type="daterange"
               placement="bottom-start"
               placeholder="选择日期"
               class="w200 mr10"
+              clearable
             >
             </DatePicker>
           </div>
           <div class="db mr10">
-            <span>供应商:</span>
+            <span class="mr10">供应商:</span>
             <Input
-              v-model="search.guestName"
+              v-model.trim="search.guestName"
               placeholder="请输入供应商"
-              class="w200 mr10"
+              class="w120 mr10"
               clearable
             />
           </div>
           <div class="db mr10">
-            <span>配件编码/名称/内码:</span>
+            <span class="mr10">配件编码/名称/内码:</span>
             <Input
-              v-model="search.partName"
+              v-model.trim="search.partName"
               placeholder="请输入配件编码/名称/内码"
-              class="w200 mr10"
+              class="w120 mr10"
               clearable
             />
           </div>
           <div class="db mr10">
-            <span>品牌:</span>
+            <span class="mr10">品牌:</span>
             <Select
-              class="w240"
+              class="w120"
               clearable
               label-in-value
               filterable
-              remote
-              :remote-method="partBrandRemote"
               @on-change="select1"
-              v-model="search.partBrand"
+              v-model.trim="search.partBrand"
               placeholder="请选择品牌"
             >
-              <Option
-                v-for="item in bandArr"
-                :value="item.label"
-                :key="item.id"
-              >{{ item.label }}</Option
-              >
+              <Option v-for="item in bandArr" :value="item.label" :key="item.id">{{ item.label }}</Option>
             </Select>
           </div>
           <div class="db mr10">
-            <span>理赔单号:</span>
+            <span class="mr10">理赔单号:</span>
             <Input
-              v-model="search.serviceId"
+              v-model.trim="search.serviceId"
               placeholder="请输入理赔单号"
               class="w120"
               clearable
             />
           </div>
           <div class="db mr10">
-            <span>索赔编号:</span>
+            <span class="mr10">索赔编号:</span>
             <Input
-              v-model="search.code"
+              v-model.trim="search.claimDemageCode"
               placeholder="请输入索赔编号"
               class="w120"
               clearable
@@ -82,13 +76,51 @@
   </section>
 </template>
 <script lang="ts">
-import {Vue, Component, Watch} from "vue-property-decorator";
-export default class panne extends Vue {
-  private search:any={
-    orderDate:[],
-    guestName:"",
-    partName:"",
+  import {Vue, Component, Watch} from "vue-property-decorator";
+  import {getBrandList} from "@/view/reportForm/until.js"
+  import {ToDayStr} from "@/components/getDate/index_bill.js"
+  import moment from "moment";
+
+  export default class panne extends Vue {
+    private search: any = {
+      orderDate: ToDayStr(),
+      guestName: "",
+      partName: "",
+      partBrand: "",
+      serviceId: "",
+      claimDemageCode: ""
+    }//搜索条件
+    private bandArr: Array<any> = new Array<any>();//品牌数组
+
+
+    async mounted() {
+      this.bandArr = await getBrandList(this.search.partBrand)
+    }
+    private getDataQuick(v){
+      this.search.orderDate = v;
+    }
+    private async partBrandRemote(query: string) {
+      this.bandArr = await getBrandList(query)
+    }
+    private query() {
+      let data:any = {};
+      for (let key in this.search) {
+        if (this.search[key]) {
+          if (key == "orderDate") {
+            data.orderStartDate =this.search["orderDate"][0]!=""?
+              moment(this.search["orderDate"][0]).startOf('day').format("YYYY-MM-DD HH:mm:ss"):""
+            data.orderEndDate =this.search["orderDate"][1]!=""?
+              moment(this.search["orderDate"][1]).endOf('day').format("YYYY-MM-DD HH:mm:ss"):""
+          } else {
+            data[key] = this.search[key];
+          }
+        }
+      }
+      this.$emit("search", data);
+    }
+    private exportxls(){
+      this.$emit("export")
+    }
   }
-}
 </script>
 <style lang="less"></style>
---
+++ src/view/afterSale/claimSheet/claimSupplier/components/table.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="oper-top">
+    <div class="flex">
+      <div class="wlf">
+        <Button type="default" @click="claim(1)">原货退还</Button>
+        <Button type="default" @click="claim(2)">换货处理</Button>
+        <Button type="default" @click="claim(3)">退货处理</Button>
+        <Button type="default" @click="claim(4)">原物销毁</Button>
+      </div>
+    </div>
+    <vxe-table
+      border
+      auto-resize
+      resizable
+      :data="claimSupplierData"
+      size="mini"
+      show-overflow="title"
+      @checkbox-all="claimSupplierSel"
+      @checkbox-change="claimSupplierSel"
+      @checkbox-cancel="claimSupplierSel"
+    >
+      <vxe-table-column type="seq" title="序号" width="40"></vxe-table-column>
+      <vxe-table-column type="selection" width="50"></vxe-table-column>
+      <vxe-table-column field="orderDate" title="提交日期" width="100"></vxe-table-column>
+      <vxe-table-column field="serviceId" title="理赔单号" width="100"></vxe-table-column>
+      <vxe-table-column field="partInnerId" title="配件内码" width="100"></vxe-table-column>
+      <vxe-table-column field="partCode" title="配件编码" width="100"></vxe-table-column>
+      <vxe-table-column field="partName" title="配件名称" width="100"></vxe-table-column>
+      <vxe-table-column field="oemCode" title="OEM码" width="100"></vxe-table-column>
+      <vxe-table-column field="partBrand" title="品牌" width="80"></vxe-table-column>
+      <vxe-table-column field="afterSaleReason" title="理赔原因" width="120"></vxe-table-column>
+      <vxe-table-column field="claimDemageCode" title="索赔编号" width="120"></vxe-table-column>
+      <vxe-table-column field="afterSaleQty" title="理赔数量" width="80"></vxe-table-column>
+      <vxe-table-column field="processedQty" title="已处理数量" width="80"></vxe-table-column>
+      <vxe-table-column field="untreatedQty" title="未处理数量" width="80"></vxe-table-column>
+      <vxe-table-column field="treatedThisQty" title="本次处理数量" width="80">
+        <template v-slot="{row}">
+          <vxe-input type="integer" v-model="row.treatedThisQty" :min="1" :max="row.untreatedQty"/>
+        </template>
+      </vxe-table-column>
+      <vxe-table-column field="remark" title="备注" width="80">
+        <template v-slot="{row}">
+          <vxe-input type="string" v-model="row.remark" maxlength="50"/>
+        </template>
+      </vxe-table-column>
+    </vxe-table>
+  </section>
+</template>
+<script lang="ts">
+  import {Vue, Component, Watch} from "vue-property-decorator";
+  import moment from "moment";
+  export default class tabOne extends Vue{
+    private claimSupplierData:Array<any>=new Array<any>();
+    private body:any={};
+    private page:any={};
+    private claimSupplierSel(){
+
+    }
+    private claim(type:number){
+
+    }
+  }
+</script>
+<style lang="less"></style>
