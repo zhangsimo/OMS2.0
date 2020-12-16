918e13ee6f2a6e68ac9068df31ef124d1a445a51
hefeng <hf15208320723@163.com>
2020-12-16 14:57:58 +0800

    现金流量表资金统计表初始化查询区域

--- src/view/settlementManagement/Statistical/cashflow.vue
+++ src/view/settlementManagement/Statistical/cashflow.vue
@@ -175,7 +175,6 @@ export default {
     this.dates = this.$refs.quickDate.val;
     this.getArea();
     this.getShop();
-    this.query();
   },
   computed:{
     selectShopList(){
@@ -267,6 +266,7 @@ export default {
         if (this.$store.state.user.userData.shopkeeper != 0) {
           this.getThisArea(); //获取当前门店地址
         }
+        this.query()
       }
     },
     //导出
