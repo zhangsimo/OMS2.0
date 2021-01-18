425265f884ed17dcdb71022ef03b3d661b1cab72
“yangkaifeng” <yangkaifeng@g-part.cn>
2021-01-18 16:24:19 +0800

    注册页面=》手机号码校验更新

--- src/view/base/register/register.vue
+++ src/view/base/register/register.vue
@@ -392,7 +392,7 @@
       const validatePhone = (rule, value, callback) => {
             if (!value) {
                 return callback(new Error('手机号不能为空'));
-            } else if (!/^1[345789]\d{9}$/.test(value)) {
+            } else if (!/^\d{3}-\d{8}|\d{4}-\{7,8}$/.test(value)) {
                 callback(new Error('手机号格式不正确'));
             } else {
                 callback();
