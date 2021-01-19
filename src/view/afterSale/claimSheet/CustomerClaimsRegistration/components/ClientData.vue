c0ea5245955d48c5f197b38af56dc83677c80849
“yangkaifeng” <yangkaifeng@g-part.cn>
2021-01-19 13:46:27 +0800

    手机号格式不正确

--- src/view/afterSale/claimSheet/CustomerClaimsRegistration/components/ClientData.vue
+++ src/view/afterSale/claimSheet/CustomerClaimsRegistration/components/ClientData.vue
@@ -428,7 +428,7 @@ export default {
     const contactorTel = (rule, value, callback) => {
       if (!value) {
         return callback(new Error("手机号不能为空"));
-      } else if (!/^1[345789]\d{9}$/.test(value)) {
+      } else if (!/^1[3456789]\d{9}$/.test(value)) {
         callback(new Error("手机号格式不正确"));
       } else {
         callback();
